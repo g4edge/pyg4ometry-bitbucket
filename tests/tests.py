a9569bf6b762f1159e82b1e62fe5489dcb765d16
swalker92 <zwap082@live.rhul.ac.uk>
2017-02-25 12:46:05 +0000

    test previously used example input, can now be overridden
    e.g. python runtests.py some-other.inp

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,20 @@
 import pyfluka
+import sys
+from IPython import embed
 
-def visiting():
+def visiting(argv = None):
     '''
-    A test for visiting
+    A test for visiting a file.
     '''
-    model = pyfluka.Model.Model("/Users/Stuart/Physics/Packages/python-tools/pyfluka/tests/fluka_test_input.inp")
+
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+    else:
+        file="/tests/fluka_test_input.inp"
+
+
+    model = pyfluka.Model.Model("/Users/Stuart/Physics/Packages"
+                                "/python-tools/pyfluka/" + file)
+
+if __name__ == '__main__':
+    visiting(sys.argv)
