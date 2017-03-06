975fe2183d2f34249cd3b26d5ef01df6dc3958f9
swalker92 <zwap082@live.rhul.ac.uk>
2017-03-06 15:58:59 +0000

    Visitor and associated classes for simple cases.
    _UnaryGDMLSolid bridges the gap between Fluka Bodies and GDML solids/volumes.

--- Model.py
+++ Model.py
@@ -1,7 +1,9 @@
 from . import Parser
 import antlr4 as _antlr4
-import pygdml as pygdml
+import pygdml as _pygdml
+import Body
 from math import pi
+from collections import namedtuple
 
 class Model(object):
 
@@ -21,13 +23,13 @@ class Model(object):
         walker = _antlr4.ParseTreeWalker()
         walker.walk(assignment_listener, tree)
 
-        visitor = _FlukaRegionVisitor()
-        visitor.visit(tree)
 
         self._get_listener_assignments(assignment_listener)
         self.report_body_count()
         self._convert_bodies_to_gdml_solids()
 
+        visitor = _FlukaRegionVisitor(self.bodies, self.materials)
+        visitor.visit(tree)
     def convert_model_to_gdml(self):
         pass
 
@@ -144,39 +146,151 @@ class _FlukaAssignmentListener(Parser.FlukaParserListener):
 
 class _FlukaRegionVisitor(Parser.FlukaParserVisitor.FlukaParserVisitor):
 
-    def __init__(self):
-        pass
+    def __init__(self, bodies, materials):
+        self.bodies = bodies
+        self.materials = materials
+        w = _pygdml.solid.Box("world", 10000, 10000, 10000)
+        self.world_volume = _pygdml.Volume(
+            [0,0,0], [0,0,0], w, "world-volume",
+            None, 1, False, "G4_Galatic")
 
-    def visitBooleanExpression(self, ctx):
-        pass
+        self.regions = {}
+
+    def visitRegion(self, ctx):
+
+        # region_solid = self.visit(self.booleanexpression())
+        region_solid = self.visitChildren(ctx)
+        region_centre_x = region_solid.centre.x
+        region_centre_y = region_solid.centre.y
+        region_centre_z = region_solid.centre.z
+        region_centre = [region_centre_x,
+                         region_centre_y,
+                         region_centre_z]
+        region_gdml = region_solid.solid
+        region_name = ctx.RegionName().getText()
+
+        if region_solid.operator == '+':
+            placement = _pygdml.volume.Volume(
+                [0,0,0], region_centre, region_gdml, region_name,
+                self.world_volume, 1, False, "copper")
+
+    def visitUnaryAndBoolean(self, ctx):
+        left_solid = self.visit(ctx.unaryExpression())
+        right_solid = self.visit(ctx.booleanExpression())
+        return left_solid.combine(right_solid)
+
+    def visitUnaryExpression(self, ctx):
+        body_name = ctx.ID().getText()
+        body = self.bodies[body_name]
+        gdml_solid = body.get_as_gdml_solid()
+        body_centre = body.get_coordinates_of_centre()
+
+        if ctx.Plus():
+            return _UnaryGDMLSolid(gdml_solid, '+', body_centre)
+        else:
+            return _UnaryGDMLSolid(gdml_solid, '-', body_centre)
 
 
+class _UnaryGDMLSolid(object):
     '''
+    A gdml solid with a unary operator from Fluka and a position.
     '''
 
+    def __init__(self, pygdml_solid, operator, centre):
+        self.solid = pygdml_solid
+        self.operator = operator
+        self.centre = centre
 
+    def combine(self, other):
         '''
+        Combine two _UnaryGDMLSolids, returning the third resultant.
         '''
 
+        if self.operator == '+' and other.operator == '+':
+            return self._combine_plus_plus(other)
+
+
+    def _combine_plus_minus(self, pygdml_solid):
+        name = self.solid.name
+        other_name = other.solid.name
+        output_name = "(" + name + "_intersection_" + other_name + ")"
+
+        other_transformation = self._get_transformation(other)
+
+        output_solid = _pygdml.solid.Subtraction(output_name,
+                                                 self.solid,
+                                                 other.solid,
+                                                 other_transformation)
+        output_operator = '+'
+        output_centre = self.centre
+
+        return _UnaryGDMLSolid(resultant_solid,
+                               resultant_operator,
+                               resultant_centre)
+
+
+    def _combine_plus_plus(self, pygdml_solid):
+
+        name = self.solid.name
+        other_name = other.solid.name
+        output_name = "(" + name + "_intersection_" + other_name + ")"
+
+        other_transformation = self._get_transformation(other)
+
+        output_solid = _pygdml.solid.Intersection(output_name,
+                                                  self.solid,
+                                                  other.solid,
+                                                  other_transformation)
+        output_operator = '+'
+        output_centre = self.centre
+
+        return _UnaryGDMLSolid(resultant_solid,
+                               resultant_operator,
+                               resultant_centre)
+
+    def _combine_minus_minus(self, other):
+        name = self.solid.name
+        other_name = other.solid.name
+        output_name = "(" + name + "_union_" + other_name + ")"
+
+        other_transformation = self._get_transformation(other)
+
+        output_operator = '-'
+        output_centre = self.centre
+
+        output_solid = _pygdml.Union(output_name,
+                                     self.solid,
+                                     other.solid,
+                                     other_transformation)
+
+
+    def _get_transformation(self, other):
+        # other_transformation is the transformation applied to the
+        # second volume w.r.t the first, which is situated at (0,0,0),
+        # when peforming the boolean operation
+        centre =  _get_tuple_in_mm(self.centre)
+        other_centre =  _get_tuple_in_mm(other.centre)
+
+        offset_x = other_centre.x - centre.x
+        offset_y = other_centre.y - centre.y
+        offset_z = other_centre.z - centre.z
+
+        other_translation = [offset_x, offset_y, offset_z]
+        other_rotation = [0,0,0]
+        other_transformation = [other_rotation, other_translation]
+
+        return other_transformation
 
 
 
 
+def _get_tuple_in_cm(input_tuple):
+    '''
+    input_tuple:  a namedtuple.
+    returns namedtuple of identical shape but with mm instead
+    '''
+    mm = 10.0
+    data = [i * mm for i in input_tuple]
+    self._DataType = namedtuple("Data", input_tuple._fields)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return _DataType(*data)
