32e1031ae7821c29e2c3ee622ab0158c5a19aacc
swalker92 <zwap082@live.rhul.ac.uk>
2017-03-14 18:12:26 +0000

    Added simple docstring for pyfluka package

--- __init__.py
+++ __init__.py
@@ -4,3 +4,15 @@ import Model
 import Parser
 
 __all__ = ['Reader','CSG']
+
+
+"""
+NAME
+pyfluka - Among among other things, this can be used to store a Fluka model
+to be stored as a python object.  In addition to basic introspection
+features, it also supports the conversion of the regions to GDML
+volumes.
+
+Fluka uses a right-handed coordinate system.
+
+"""
