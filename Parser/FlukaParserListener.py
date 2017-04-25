39c625a2f6f812bb42497f03e6d9d40ca0cf0e3a
swalker92 <zwap082@live.rhul.ac.uk>
2017-04-25 13:18:24 +0100

    generated listeners and visitors

---
+++ Parser/FlukaParserListener.py
@@ -0,0 +1,240 @@
+# Generated from FlukaParser.g4 by ANTLR 4.6
+from antlr4 import *
+
+# This class defines a complete listener for a parse tree produced by FlukaParser.
+class FlukaParserListener(ParseTreeListener):
+
+    # Enter a parse tree produced by FlukaParser#model.
+    def enterModel(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#model.
+    def exitModel(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#command.
+    def enterCommand(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#command.
+    def exitCommand(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#SimpleMaterial.
+    def enterSimpleMaterial(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#SimpleMaterial.
+    def exitSimpleMaterial(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#CompoundMaterial.
+    def enterCompoundMaterial(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#CompoundMaterial.
+    def exitCompoundMaterial(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#material.
+    def enterMaterial(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#material.
+    def exitMaterial(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#compound.
+    def enterCompound(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#compound.
+    def exitCompound(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#geoBegin.
+    def enterGeoBegin(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#geoBegin.
+    def exitGeoBegin(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#otherKeywords.
+    def enterOtherKeywords(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#otherKeywords.
+    def exitOtherKeywords(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#geocards.
+    def enterGeocards(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#geocards.
+    def exitGeocards(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#GeometryDirective.
+    def enterGeometryDirective(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#GeometryDirective.
+    def exitGeometryDirective(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#BodyDefSpaceDelim.
+    def enterBodyDefSpaceDelim(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#BodyDefSpaceDelim.
+    def exitBodyDefSpaceDelim(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#BodyDefPunctDelim.
+    def enterBodyDefPunctDelim(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#BodyDefPunctDelim.
+    def exitBodyDefPunctDelim(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#region.
+    def enterRegion(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#region.
+    def exitRegion(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#zoneUnion.
+    def enterZoneUnion(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#zoneUnion.
+    def exitZoneUnion(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#zone.
+    def enterZone(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#zone.
+    def exitZone(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#subZone.
+    def enterSubZone(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#subZone.
+    def exitSubZone(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#singleUnary.
+    def enterSingleUnary(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#singleUnary.
+    def exitSingleUnary(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#unaryAndBoolean.
+    def enterUnaryAndBoolean(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#unaryAndBoolean.
+    def exitUnaryAndBoolean(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#unaryAndSubZone.
+    def enterUnaryAndSubZone(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#unaryAndSubZone.
+    def exitUnaryAndSubZone(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#binaryUnion.
+    def enterBinaryUnion(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#binaryUnion.
+    def exitBinaryUnion(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#unaryExpression.
+    def enterUnaryExpression(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#unaryExpression.
+    def exitUnaryExpression(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#geoDirective.
+    def enterGeoDirective(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#geoDirective.
+    def exitGeoDirective(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#expansion.
+    def enterExpansion(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#expansion.
+    def exitExpansion(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#translat.
+    def enterTranslat(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#translat.
+    def exitTranslat(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#transform.
+    def enterTransform(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#transform.
+    def exitTransform(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by FlukaParser#lattice.
+    def enterLattice(self, ctx):
+        pass
+
+    # Exit a parse tree produced by FlukaParser#lattice.
+    def exitLattice(self, ctx):
+        pass
+
+
