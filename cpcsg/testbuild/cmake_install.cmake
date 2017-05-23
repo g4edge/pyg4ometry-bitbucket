53ef0ca74b0b44b55d9ae98f30bf1de1b3f8210f
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2017-05-23 08:03:50 +0100

    Merge

--- cpcsg/testbuild/cmake_install.cmake
+++ cpcsg/testbuild/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/alistairbutcher/Dropbox/LZ/pygeometry/cpcsg
+# Install script for directory: /Users/sboogert/Physics/coderepos/pygeometry/cpcsg
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/alistairbutcher/Dropbox/LZ/pygeometry/cpcsg/testbuild/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/sboogert/Physics/coderepos/pygeometry/cpcsg/testbuild/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
