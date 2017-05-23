04b93a9c7a7510f08c643d47ff2adf5e51a55861
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2017-05-23 17:44:38 +0100

    Added first C++ solids

---
+++ cpcsg/Solids.C
@@ -0,0 +1,44 @@
+#include "Solids.h"
+
+CSG Solids::Box(double dx, double dy, double dz) {
+  std::vector<Polygon*> polygons;
+
+  Vertex v1 = Vertex(Vector(-1,-1,-1));
+  Vertex v2 = Vertex(Vector(-1, 1,-1));
+  Vertex v3 = Vertex(Vector( 1, 1,-1));
+  Vertex v4 = Vertex(Vector( 1,-1,-1));
+
+  Vertex v5 = Vertex(Vector(-1,-1, 1));
+  Vertex v6 = Vertex(Vector(-1, 1, 1));
+  Vertex v7 = Vertex(Vector( 1, 1, 1));
+  Vertex v8 = Vertex(Vector( 1,-1, 1));
+  
+  std::vector<Vertex> f1;
+  f1.push_back(v1);
+  f1.push_back(v2);
+  f1.push_back(v3);
+  f1.push_back(v4);
+
+  std::vector<Vertex> f2;
+  f1.push_back(v5);
+  f1.push_back(v6);
+  f1.push_back(v7);
+  f1.push_back(v8);
+
+  std::vector<Vertex> f3;
+  f1.push_back(v1);
+  f1.push_back(v5);
+  f1.push_back(v8);
+  f1.push_back(v4);
+
+  Polygon *p1 = new Polygon(f1,NULL);
+  Polygon *p2 = new Polygon(f2,NULL);
+  Polygon *p3 = new Polygon(f3,NULL);
+   
+  std::vector<Polygon*> p;
+  p.push_back(p1);
+  p.push_back(p2);
+  p.push_back(p3);
+
+  return *(CSG::fromPolygons(p));
+}
---
+++ cpcsg/Solids.h
@@ -0,0 +1,9 @@
+#include "CSG.h"
+#include "Polygon.h"
+#include "Vertex.h"
+#include "Vector.h"
+
+class Solids {
+ public:
+  static CSG Box(double dx,double dy,double dz);
+};
