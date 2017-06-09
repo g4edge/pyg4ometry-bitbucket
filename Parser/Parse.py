06efcc0e92f301dc89ecebd95521d721d1e0d77d
swalker92 <zwap082@live.rhul.ac.uk>
2017-06-09 21:58:12 +0100

    functions in parser for processing file string
    split into geometry and not geometry, use antlr4 to parse the
    geometry.  use string processing to parse everything else (everything
    else is just statements)

--- Parser/Parse.py
+++ Parser/Parse.py
@@ -10,10 +10,14 @@ def Parse(path):
         raise IOError("File not found: %s" % path)
 
     with open(path, 'r') as f:
-        file_string = f.read()
+        lines = f.readlines()
+
+    lines = _remove_col0_comments(lines)
+    not_geometry, geometry = _separate_geometry(lines)
 
     # Create _ANTLR4 char stream from processed model string
-    istream = _antlr4.InputStream(file_string)
+    # geometry is a list of strings here, so join as single string.
+    istream = _antlr4.InputStream(''.join(geometry))
 
     # Tokenise character stream
     lexed_input = FlukaLexer(istream)
@@ -29,6 +33,28 @@ def Parse(path):
 
     return tree
 
+def _remove_col0_comments(lines):
+    # Loop over all lines and remove those that have an '*' in column
+    # 0.  The other comment type is '!', which doesn't necessarily
+    # have to be in column 0.
+    for index, line in enumerate(lines):
+        if line[0] == '*' or line[0] == '!':
+            del lines[index]
+    return lines
+
+def _separate_geometry(lines):
+    # Get the two indices of geobegin and geoend
+    geo_begin_index = (i for i, line in enumerate(lines)
+                       if line.startswith("GEOBEGIN")).next()
+    geo_end_index = (i for i, line in enumerate(lines)
+                     if line.startswith("GEOEND")).next()
+    # slice and return
+    before_geometry = lines[:geo_begin_index]
+    geometry = lines[geo_begin_index:geo_end_index + 1]
+    after_geometry = lines[geo_end_index + 1:]
+    not_geometry = before_geometry + after_geometry
+
+    return not_geometry, geometry
 
 def VisitModel(tree):
 
