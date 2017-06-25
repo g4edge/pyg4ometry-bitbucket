24f2085b26b74d0ceac2f2ceefea55ab4639f5ac
swalker92 <zwap082@live.rhul.ac.uk>
2017-06-25 19:50:45 +0100

    line numbers now preserved before parsing
    this way the line number raised by the parser corresponds to the input
    file itself

--- Parser/Parse.py
+++ Parser/Parse.py
@@ -12,7 +12,6 @@ def Parse(path):
     with open(path, 'r') as f:
         lines = f.readlines()
 
-    lines = _remove_col0_comments(lines)
     not_geometry, geometry = _separate_geometry(lines)
 
     # Create _ANTLR4 char stream from processed model string
@@ -33,16 +32,14 @@ def Parse(path):
 
     return tree
 
-def _remove_col0_comments(lines):
-    # Loop over all lines and remove those that have an '*' in column
-    # 0.  The other comment type is '!', which doesn't necessarily
-    # have to be in column 0.
-    for index, line in enumerate(lines):
-        if line[0] == '*' or line[0] == '!':
-            del lines[index]
-    return lines
 
 def _separate_geometry(lines):
+    """Separate the model into two parts, lines describing the
+    geometry, and lines describing what is not the geometry.  In both
+    cases, the other is commented out rather than removed.  This is
+    useful as the line numbering is retained.
+
+    """
     # Get the two indices of geobegin and geoend
     geo_begin_index = (i for i, line in enumerate(lines)
                        if line.startswith("GEOBEGIN")).next()
@@ -57,10 +54,18 @@ def _separate_geometry(lines):
     else: # no break
         geo_begin_index += 1
 
+    # Split the model into 3 parts
     before_geometry = lines[:geo_begin_index + 1]
     geometry = lines[geo_begin_index + 1:geo_end_index]
     after_geometry = lines[geo_end_index:]
-    not_geometry = before_geometry + after_geometry
+
+    # Create list of equal length of commented lines to preserve line numbers
+    commented_before_geometry = ["*\n" for line in before_geometry]
+    commented_geometry = ["*\n" for line in geometry]
+    commented_after_geometry = ["*\n" for line in after_geometry]
+    # Assemble and return the two sets:
+    geometry = commented_before_geometry + geometry + commented_after_geometry
+    not_geometry = before_geometry + commented_geometry + after_geometry
     return not_geometry, geometry
 
 def VisitModel(tree):
