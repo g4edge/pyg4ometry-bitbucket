2aa237ca930909ae015aeed79a7e90bba064c341
swalker92 <zwap082@live.rhul.ac.uk>
2017-06-25 20:14:32 +0100

    improved formatting in Parser.py

--- Parser/Parse.py
+++ Parser/Parse.py
@@ -1,28 +1,30 @@
-import subprocess as _sp
-import os.path as _path
-import antlr4 as _antlr4
+"""Functions for processing and parsing Fluka files."""
+import os.path
 
-from FlukaLexer import FlukaLexer
-from FlukaParser import FlukaParser
+import antlr4
+
+from .FlukaLexer import FlukaLexer
+from .FlukaParser import FlukaParser
 
 def Parse(path):
-    if not _path.exists(path):
+    """Return the antlr4 syntax tree for the given file."""
+    if not os.path.exists(path):
         raise IOError("File not found: %s" % path)
 
-    with open(path, 'r') as f:
-        lines = f.readlines()
+    with open(path, 'r') as fluka_file:
+        lines = fluka_file.readlines()
 
-    not_geometry, geometry = _separate_geometry(lines)
+    _, geometry = _separate_geometry(lines)
 
-    # Create _ANTLR4 char stream from processed model string
+    # Create ANTLR4 char stream from processed model string
     # geometry is a list of strings here, so join as single string.
-    istream = _antlr4.InputStream(''.join(geometry))
+    istream = antlr4.InputStream(''.join(geometry))
 
     # Tokenise character stream
     lexed_input = FlukaLexer(istream)
 
     # Create a buffer of tokens from lexer
-    tokens = _antlr4.CommonTokenStream(lexed_input)
+    tokens = antlr4.CommonTokenStream(lexed_input)
 
     # Create a parser that reads from stream of tokens
     parser = FlukaParser(tokens)
@@ -46,9 +48,13 @@ def _separate_geometry(lines):
     geo_end_index = (i for i, line in enumerate(lines)
                      if line.startswith("GEOEND")).next()
 
+    body_types = ["ARB", "BOX", "ELL", "PLA", "QUA", "RAW",
+                  "RCC", "REC", "RPP", "SPH", "TRC", "WED",
+                  "XCC", "XEC", "XYP", "XZP", "YCC", "YEC",
+                  "YZP", "ZCC", "ZEC"]
     # Check if line after geobegin is a body definition, if not then
     # it is part of the GEOBEGIN card, and move begin_index onwards one.
-    for body_type in _body_types:
+    for body_type in body_types:
         if lines[geo_begin_index + 1].startswith(body_type):
             break
     else: # no break
@@ -60,22 +66,11 @@ def _separate_geometry(lines):
     after_geometry = lines[geo_end_index:]
 
     # Create list of equal length of commented lines to preserve line numbers
-    commented_before_geometry = ["*\n" for line in before_geometry]
-    commented_geometry = ["*\n" for line in geometry]
-    commented_after_geometry = ["*\n" for line in after_geometry]
+    commented_before_geometry = len(before_geometry) * ["*\n"]
+    commented_geometry = len(geometry) * ["*\n"]
+    commented_after_geometry = len(after_geometry) * ["*\n"]
     # Assemble and return the two sets:
     geometry = commented_before_geometry + geometry + commented_after_geometry
     not_geometry = before_geometry + commented_geometry + after_geometry
+
     return not_geometry, geometry
-
-def VisitModel(tree):
-
-    if visitor != default:
-        pass
-
-    visitor = FlukaTreeVisitor()
-
-_body_types = ["ARB", "BOX", "ELL", "PLA", "QUA", "RAW",
-               "RCC", "REC", "RPP", "SPH", "TRC", "WED",
-               "XCC", "XEC", "XYP", "XZP", "YCC", "YEC",
-               "YZP", "ZCC", "ZEC"]
