e413ceff8ec7901534cdc2e60010cf9a157f3a7d
Simon Williams <Simon.Williams.2015@live.rhul.ac.uk>
2017-06-27 15:02:35 +0100

    New meshing algorithm for TwistedBox sold and test added

--- geant4/solid/TwistedBox.py
+++ geant4/solid/TwistedBox.py
@@ -2,13 +2,93 @@ from SolidBase import SolidBase as _SolidBase
 from pygeometry.pycsg.core import CSG as _CSG
 from pygeometry.pycsg.geom import Vector as _Vector
 from pygeometry.pycsg.geom import Vertex as _Vertex
+from pygeometry.pycsg.geom import Plane as _Plane
 from pygeometry.pycsg.geom import Polygon as _Polygon
 from pygeometry.geant4.Registry import registry as _registry
 from pygeometry.geant4.solid.Wedge import Wedge as _Wedge
 import numpy as _np
 
-class TwistedBox(_SolidBase) :
-    def __init__(self, name, twistedangle, pDx, pDy, pDz, refine=0) :
+class TwoVector(object):
+    def __init__(self, xIn, yIn):
+        self.x = xIn
+        self.y = yIn
+
+    def Rotated(self, angle):
+        # do rotation
+        xr = self.x*_np.cos(angle) - self.y*_np.sin(angle)
+        yr = self.x*_np.sin(angle) + self.y*_np.cos(angle)
+        return TwoVector(xr,yr)
+
+    def __repr__(self):
+        s = '(' + str(self.x) + ', ' + str(self.y) + ')'
+        return s
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError("Invalid index "+str(index))
+
+    def __add__(self, other):
+        if type(other) == TwoVector:
+            return TwoVector(self.x + other.x, self.y + other.y)
+        elif type(other) == float or type(other) == int:
+            return TwoVector(self.x + other, self.y + other)
+        else:
+            raise ValueError("unsupported type " + str(type(other)))
+    
+    def __sub__(self, other):
+        if type(other) == TwoVector:
+            return TwoVector(self.x - other.x, self.y - other.y)
+        elif type(other) == float or type(other) == int:
+            return TwoVector(self.x - other, self.y - other)
+        else:
+            raise ValueError("unsupported type " + str(type(other)))
+              
+    def __mul__(self, other):
+        if type(other) == float or type(other) == int:
+            return TwoVector(self.x * other, self.y * other)
+        else:
+            raise ValueError("unsupported type " + str(type(other)))
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+              
+    def __div__(self, other):
+        if type(other) == float or type(other) == int:
+            return TwoVector(self.x / other, self.y / other)
+        else:
+            raise ValueError("unsupported type " + str(type(other)))
+    
+class Layer(object):
+    def __init__(self, p1, p2, p3, p4, z):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+        self.z  = z
+
+    def Rotated(self, angle):
+        result = Layer(self.p1.Rotated(angle),
+                       self.p2.Rotated(angle),
+                       self.p3.Rotated(angle),
+                       self.p4.Rotated(angle),
+                       self.z)
+        return result
+
+    def __repr__(self):
+        s = 'Layer<'
+        s += str(self.p1) + ', '
+        s += str(self.p2) + ', '
+        s += str(self.p3) + ', '
+        s += str(self.p4) + ', '
+        s += ' z = ' + str(self.z) + '>'
+        return s
+
+class TwistedBox2(_SolidBase) :
+    def __init__(self, name, twistedangle, pDx, pDy, pDz, nslice=2, refine=0) :
         """
         Constructs a tube of elliptical cross-section. 
 
@@ -27,60 +107,102 @@ class TwistedBox(_SolidBase) :
         self.pDx    = pDx
         self.pDy    = pDy
         self.pDz    = pDz
+        self.nslice  = nslice
         self.refine = refine
-        _registry.addSolid(self)                
+        _registry.addSolid(self)
 
+
+    def makeLayers(self, p1, p2, p3, p4, pDz, theta, nslice):
+        dz = 2*pDz/nslice
+        dtheta = theta/nslice
+        z = -pDz
+
+        layers = []
+        
+        bottom = Layer(p1,p2,p3,p4, z)
+        bottom = bottom.Rotated(-theta*0.5) #overwrite
+        layers.append(bottom)
+        
+        for i in range(nslice):
+            l = layers[-1].Rotated(dtheta) # returns rotated copy
+            z += dz # increment z
+            l.z = z # fix z
+            layers.append(l)
+
+        return layers
+
+    def makeFaceFromLayer(self, layer):
+        pols = []
+        l = layer
+        for p in [l.p1, l.p2, l.p3, l.p4]:
+            pols.append(_Vertex(_Vector(p.x, p.y, l.z), None))
+        return _Polygon(pols)
+
+    def makeSide(self, pal, pbl, pau, pbu, zl, zu, nsl):
+        """
+        p = point
+        a = first
+        b = second
+        u = upper
+        l = lower
+        """
+        pols = []
+        for i in range(nsl):
+            pll = pal + i     * (pbl - pal) / nsl
+            plr = pal + (i+1) * (pbl - pal) / nsl
+            pul = pau + i     * (pbu - pau) / nsl
+            pur = pau + (i+1) * (pbu - pau) / nsl
+        
+            pol1 = _Polygon([_Vertex(_Vector(pll.x, pll.y, zl), None),
+                             _Vertex(_Vector(pul.x, pul.y, zu), None),
+                             _Vertex(_Vector(pur.x, pur.y, zu), None)])
+            pols.append(pol1)
+            
+            pol2 = _Polygon([_Vertex(_Vector(plr.x, plr.y, zl), None),
+                             _Vertex(_Vector(pll.x, pll.y, zl), None),
+                             _Vertex(_Vector(pur.x, pur.y, zu), None)])
+            pols.append(pol2)
+        return pols
+        
     def pycsgmesh(self):
-        vert_crd_up = [[-self.pDx, -self.pDy, self.pDz],[self.pDx, -self.pDy, self.pDz],[self.pDx, self.pDy, self.pDz],[-self.pDx, self.pDy, self.pDz]]
-        vert_crd_dn = [[-self.pDx, -self.pDy, -self.pDz],[-self.pDx, self.pDy, -self.pDz],[self.pDx, self.pDy, -self.pDz],[self.pDx, -self.pDy, -self.pDz]]
+        p1 = TwoVector(-self.pDx, -self.pDy)#, self.pDz]
+        p2 = TwoVector(self.pDx, -self.pDy) # self.pDz]
+        p3 = TwoVector(self.pDx, self.pDy) #self.pDz]
+        p4 = TwoVector(-self.pDx, self.pDy) # self.pDz]
 
-        vert_crd_rot = []
+        m = self.makeLayers(p1, p2, p3, p4, self.pDz, self.twistedAngle, self.nslice)
+
+        return self.meshFromLayers(m, self.nslice)
+
+    def meshFromLayers(self, layers, nsl):
+        l = layers #shortcut
+        allPolygons = []
+        polyTop = []
+        polyBottom = []      
+
+        bottom = self.makeFaceFromLayer(l[-1])
+        allPolygons.append(bottom)
         
-        for i in range(len(vert_crd_up)):
-            x_rot = vert_crd_up[i][0]*_np.cos(self.twistedAngle) - vert_crd_up[i][1]*_np.sin(self.twistedAngle)
-            y_rot = vert_crd_up[i][0]*_np.sin(self.twistedAngle) + vert_crd_up[i][1]*_np.cos(self.twistedAngle)
-            #x_rot = vert_crd_up[i][0]
-            #y_rot = vert_crd_up[i][1]
-            vert_crd_rot.append([x_rot, y_rot, vert_crd_up[i][2]])
+        for zi in range(len(l) - 1):
+            ll = l[zi]
+            ul = l[zi + 1]
             
-        vert_up = []
-        vert_dn = []
+            pols = self.makeSide(ll.p1, ll.p2, ul.p1, ul.p2, ll.z, ul.z, nsl)
+            allPolygons.extend(pols)
 
-        for i in range(len(vert_crd_dn)):
-            vert_up.append(_Vertex(_Vector(vert_crd_rot[i][0],vert_crd_rot[i][1],vert_crd_rot[i][2]), None))
-            vert_dn.append(_Vertex(_Vector(vert_crd_dn[i][0],vert_crd_dn[i][1],vert_crd_dn[i][2]), None))
-        """
-        self.mesh = _CSG.fromPolygons([_Polygon([vert_dn[0], vert_dn[1], vert_dn[2], vert_dn[3]], None),
-                                       _Polygon([vert_up[0], vert_up[1], vert_up[2], vert_up[3]], None),
-                                       _Polygon([vert_dn[0], vert_dn[3], vert_up[1], vert_up[0]], None),
-                                       _Polygon([vert_dn[1], vert_up[3], vert_up[2], vert_dn[2]], None),
-                                       _Polygon([vert_dn[0], vert_up[0], vert_up[3], vert_dn[1]], None),
-                                       _Polygon([vert_dn[3], vert_dn[2], vert_up[2], vert_up[1]], None)])
-        #_Polygon([vert_dn[3], vert_dn[2], vert_dn[1], vert_dn[0], vert_dn[3]], None),
-        #_Polygon([vert_up[0], vert_up[1], vert_up[2], vert_up[3], vert_up[0]], None),
-        for i in range(self.refine):
-            self.mesh = self.mesh.refine()
-        """
+            pols = self.makeSide(ll.p2, ll.p3, ul.p2, ul.p3, ll.z, ul.z, nsl)
+            allPolygons.extend(pols)
+
+            pols = self.makeSide(ll.p3, ll.p4, ul.p3, ul.p4, ll.z, ul.z, nsl)
+            allPolygons.extend(pols)
+
+            pols = self.makeSide(ll.p4, ll.p1, ul.p4, ul.p1, ll.z, ul.z, nsl)
+            allPolygons.extend(pols)
         
-        self.mesh = _CSG.fromPolygons([_Polygon([vert_dn[0], vert_dn[1], vert_dn[2]], None),
-                                       _Polygon([vert_dn[2], vert_dn[3], vert_dn[0]], None),
-                                       
-                                       _Polygon([vert_up[0], vert_up[1], vert_up[2]], None),
-                                       _Polygon([vert_up[2], vert_up[3], vert_up[0]], None),
-                                       
-                                       _Polygon([vert_dn[0], vert_dn[3], vert_up[1]], None),
-                                       _Polygon([vert_dn[0], vert_up[1], vert_up[0]], None),
-                                       
-                                       _Polygon([vert_dn[1], vert_up[2], vert_dn[2]], None),
-                                       _Polygon([vert_dn[1], vert_up[3], vert_up[2]], None),
-                                       
-                                       _Polygon([vert_dn[0], vert_up[0], vert_up[3]], None),
-                                       _Polygon([vert_up[3], vert_dn[1], vert_dn[0]], None),
-                                       
-                                       _Polygon([vert_dn[3], vert_dn[2], vert_up[2]], None),
-                                       _Polygon([vert_up[2], vert_up[1], vert_dn[3]], None)])
+        top = self.makeFaceFromLayer(l[0])
+        allPolygons.append(top)
+        
+        self.mesh = _CSG.fromPolygons(allPolygons)
+        
 
-        for i in range(self.refine):
-            self.mesh = self.mesh.refine()
-            
         return self.mesh
--- test/TwistedBox.py
+++ test/TwistedBox.py
@@ -1,16 +1,29 @@
 import pygeometry.geant4 as _g4
 import pygeometry.vtk as _vtk
 import pygeometry.gdml as _gdml
+import numpy as _np
 
 def pycsgmeshTest(vtkViewer = True, gdmlWriter = True) :
     _g4.registry.clear()
 
-    worldSolid   = _g4.solid.Box('worldBox', 250,250,100)
+    worldSolid   = _g4.solid.Box('worldBox', 500,500,150)
     worldLogical =  _g4.LogicalVolume(worldSolid,'G4_Galactic','worldLogical')
 
     twistedBoxSolid1    = _g4.solid.TwistedBox('twistedBox1',0.5, 30, 40, 60)
     twistedBoxLogical1  = _g4.LogicalVolume(twistedBoxSolid1,'G4_Cu','twistedBoxLogical1')
-    twistedBoxPhysical1 = _g4.PhysicalVolume([0,0,0],[0,0,0], twistedBoxLogical1,'twistedBoxPhysical1',worldLogical)
+    twistedBoxPhysical1 = _g4.PhysicalVolume([0,0,0],[200,200,0], twistedBoxLogical1,'twistedBoxPhysical1',worldLogical)
+
+    twistedBoxSolid2    = _g4.solid.TwistedBox('twistedBox2',0.2, 50, 40, 60)
+    twistedBoxLogical2  = _g4.LogicalVolume(twistedBoxSolid2,'G4_Cu','twistedBoxLogical2')
+    twistedBoxPhysical2 = _g4.PhysicalVolume([0,0,0],[200,0,0], twistedBoxLogical2,'twistedBoxPhysical2',worldLogical)
+
+    twistedBoxSolid3    = _g4.solid.TwistedBox('twistedBox3',1.5, 20, 30, 40)
+    twistedBoxLogical3  = _g4.LogicalVolume(twistedBoxSolid3,'G4_Cu','twistedBoxLogical3')
+    twistedBoxPhysical3 = _g4.PhysicalVolume([0,0,0],[0,200,0], twistedBoxLogical3,'twistedBoxPhysical3',worldLogical)
+
+    twistedBoxSolid4    = _g4.solid.TwistedBox('twistedBox4',0.5, 30, 40, 50)
+    twistedBoxLogical4  = _g4.LogicalVolume(twistedBoxSolid4,'G4_Cu','twistedBoxLogical4')
+    twistedBoxPhysical4 = _g4.PhysicalVolume([0,0,0],[0,0,0], twistedBoxLogical4,'twistedBoxPhysical4',worldLogical)
 
     # clip the world logical volume
     worldLogical.setClip();
