9e6bc4056c004b36a12c7e775541da4fd0a7d66d
swalker92 <zwap082@live.rhul.ac.uk>
2017-07-11 18:53:07 +0100

    improved package doc

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
-"""
-pyfluka - Among among other things, this can be used to store a Fluka model
+"""pyfluka - Among among other things, this can be used to store a Fluka model
 to be stored as a python object.  In addition to basic introspection
 features, it also supports the conversion of the regions to GDML
 volumes.
 
-Fluka uses a right-handed coordinate system.
+Notes:
+
+- The only geometry format currently supported is the free, space-delimited
+format.
+- The only format currently supported for non-geometry cards is fixed format.
 
 """
 
