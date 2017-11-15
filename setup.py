da6b4ffd098b12d9e98cc90ed58bb73995e05aa5
swalker92 <zwap082@live.rhul.ac.uk>
2017-11-15 18:47:42 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=["antlr4-python2-runtime == 4.7", "numpy", "matplotlib"],
     python_requires="==2.7.*", # refer to pep440 for writing these correctly
 
-    author="Stuart D. Walker",
+    author="Stuart D Walker",
     author_email="stuart.walker.2011@live.rhul.ac.uk",
     description='View FLUKA geometry meshes and convert to GDML.',
     url='https://bitbucket.org/jairhul/pyfluka/',
