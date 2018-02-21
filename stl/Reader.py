1c57732810f5494a4b8ccd2f9cf530fb32fedc47
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2018-02-21 22:08:34 +0100

    Added options to visualise an STL file or write it directly to GDML upon loading. Will factor this functionality out of the reader in the future, but useful for now.

--- stl/Reader.py
+++ stl/Reader.py
@@ -4,9 +4,10 @@ import numpy             as _np
 import re as _re
 import warnings as _warnings
 
+import pygeometry.gdml as _gdml
 
 class Reader(object):
-    def __init__(self, filename):
+    def __init__(self, filename, visualise, writeGDML):
         super(Reader, self).__init__()
         self.filename = filename
 
@@ -15,9 +16,9 @@ class Reader(object):
         self.num_re = _re.compile(r"^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$") #Compile re to match numbers
 
         # load file
-        self.load()
+        self.load(visualise, writeGDML)
 
-    def load(self):
+    def load(self, visualise=True, writeGDML=False):
         #data  = open(self.filename, "r")
         def extractXYZ(string):
             return tuple([float(v) for v in string.split() if self.num_re.match(v)])
@@ -45,25 +46,36 @@ class Reader(object):
         name = "tess"
         tessSolid = _g4.solid.TesselatedSolid(name, self.facet_list)
 
-        worldSolid   = _g4.solid.Box('worldBox',10,10,10)
-        worldLogical = _g4.LogicalVolume(worldSolid,'G4_Galactic','worldLogical')
+        if visualise or writeGDML:
+            worldSolid   = _g4.solid.Box('worldBox',10,10,10)
+            worldLogical = _g4.LogicalVolume(worldSolid,'G4_Galactic','worldLogical')
 
-        tessLogical  = _g4.LogicalVolume(tessSolid, "Cheese", "tessLogical")
-        boxPhysical2 = _g4.PhysicalVolume([0,0,0], [0,0,0],tessLogical,'tessPhysical',worldLogical)
+            tessLogical  = _g4.LogicalVolume(tessSolid, "G4_CONCRETE", "tessLogical")
+            boxPhysical2 = _g4.PhysicalVolume([0,0,0], [0,0,0],tessLogical,'tessPhysical',worldLogical)
 
-        # clip the world logical volume
-        worldLogical.setClip();
+            # clip the world logical volume
+            worldLogical.setClip();
 
-        # register the world volume
-        _g4.registry.setWorld('worldLogical')
+            # register the world volume
+            _g4.registry.setWorld('worldLogical')
 
-        # mesh the geometry
-        m = worldLogical.pycsgmesh()
+            if visualise:
+                # mesh the geometry
+                m = worldLogical.pycsgmesh()
 
-        # view the geometry
-        v = _vtkv.Viewer()
-        v.addPycsgMeshList(m, refine=False)
-        v.view();
+                # view the geometry
+                v = _vtkv.Viewer()
+                v.addPycsgMeshList(m, refine=False)
+                v.view();
+
+            if writeGDML:
+                # write gdml
+                w = _gdml.Writer()
+                w.addDetector(_g4.registry)
+                w.write('./Tessellated.gdml')
+                w.writeGmadTester('Tessellated.gmad')
+
+        return tessSolid
 
 class _Facet():
     def __init__(self, normal=(0,0,0)):
