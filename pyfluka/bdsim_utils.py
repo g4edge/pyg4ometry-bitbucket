fba47ce8842b46f5504b24517954f1fe599e1f16
swalker92 <zwap082@live.rhul.ac.uk>
2018-04-06 13:15:30 +0100

    Got get_placement_string working
    Friendly all in one function for placing the fluka geometry.
    Previously had to do it a bit more by hand.

--- pyfluka/bdsim_utils.py
+++ pyfluka/bdsim_utils.py
@@ -6,37 +6,73 @@ import warnings
 import textwrap
 
 
-import pygdml.transformation
+import pygdml.transformation as trf
 import pyfluka.vector
 
-
-def _get_bdsim_rotation(fluka_direction, bdsim_rotation):
-    """Get the rotation for placing the external FLUKA geometry with
-    respect to the BDSIM beamline.
-
-    fluka_direction -- a vector in the FLUKA coordinate
-    system that will be aligned with the vector corresponding to the
-    rotation of (0,0,1) described in `bdsim_rotation`.  This is typically the
-    direction the beamline in FLUKA-world points in.
-    bdsim_rotation -- AxisAngle instance providing the rotation w.r.t
-    the +ve z-axis in BDSIM-world.  A typical source of such a
-    rotation is the BDSOutputROOTEventModel tree in the rootevent
-    output, where the rotation for each element w.r.t the positive
-    z-axis can be found.
+def get_placement_string(output_name,
+                         gdml_path,
+                         bdsim_point,
+                         fluka_point,
+                         fluka_bounding_box_origin,
+                         bdsim_rotation_axis,
+                         bdsim_rotation_angle,
+                         fluka_direction):
+    """
+    output_name -- the name of the output geometry placement.
+    gdml_path -- the path of the GDML file.
+    bdsim_point -- the point in BDSIM-world to coincide with
+                         `fluka_point` in BDSIM-world.
+    fluka_point -- the point in FLUKA-world to coincide with
+                         `bdsim_point` in BDSIM-world
+    fluka_bounding_box_origin -- the centre of the bounding box in
+                         FLUKA-world.  This coordinate is in the
+                         dictionary returned by
+                         pyfluka.Model.write_to_gdml.
+    bdsim_rotation_axis -- The axis component of the axis-angle
+                         rotation denoting the desired rotation in
+                         BDSIM world.
+    bdsim_rotation_angle -- The angle component of the axis-angle
+                         rotation denoting the desired rotation in
+                         BDSIM world.
+    fluka_direction -- the unit vector denoting the direction in the
+                         FLUKA world that is to be orientated to the
+                         rotation in bdsim_rotation.
 
     """
-    bdsim_rotation_matrix = (
-        pygdml.transformation.axisangle2matrix(bdsim_rotation.axis,
-                                               bdsim_rotation.angle))
-    # The BDSIM rotation is w.r.t the positive z-axis, so we pick that
-    # here.  This is because the beamline in BDSIM builds along +ve z.
-    fluka_rotation_matrix = pygdml.matrix_from(fluka_direction, [0, 0, 1])
-    net_rotation = fluka_rotation_matrix.dot(
-        bdsim_rotation_matrix.dot(fluka_rotation_matrix))
-    axis_angle = pygdml.transformation.matrix2axisangle(net_rotation)
-    axis = axis_angle[0]
-    angle = axis_angle[1]
-    return AxisAngle(axis, angle)
+    axis, angle = _get_bdsim_rotation(fluka_direction,
+                                      bdsim_rotation_axis,
+                                      bdsim_rotation_angle)
+    placement = _get_bdsim_placement(bdsim_point, fluka_point,
+                                     fluka_bounding_box_origin,
+                                     axis, angle)
+    return _build_placement_string(output_name, gdml_path, placement,
+                                   axis, angle)
+
+
+
+def _get_bdsim_rotation(fluka_direction,
+                        bdsim_rotation_axis,
+                        bdsim_rotation_angle):
+    """Get the rotation for placing the external FLUKA geometry with
+    respect to the BDSIM beamline.  The fluka_direction vector is
+    aligned to the orientation denoted by bdsim_rotation
+
+    fluka_direction --  a vector in the FLUKA coordinate system.
+    bdsim_rotation -- an axis-angle rotation in the bdsim coordinate system.
+
+    """
+    # The reason why the fluka_direction should be a unit vector and
+    # the bdsim_rotation instead an axis-angle rotation is that FLUKA
+    # uses unit vectors to denote orientation and BDSIM output stores
+    # rotations as axis-angle rotations.
+    # Vector pointing in the direction sig
+    bdsim_unit_vector = trf.axisangle2matrix(bdsim_rotation_axis,
+                                             bdsim_rotation_angle).dot(
+                                                 [0, 0, 1]).A1
+    net_rotation_matrix = trf.matrix_from(fluka_direction, bdsim_unit_vector)
+
+    axis_angle = trf.matrix2axisangle(net_rotation_matrix)
+    return axis_angle
 
 def _get_bdsim_placement(bdsim_point, fluka_point,
                          fluka_bounding_box_origin,
@@ -55,50 +91,13 @@ def _get_bdsim_placement(bdsim_point, fluka_point,
 
     """
     # Convert the axis/angle back to a rotation matrix.
-    rotation_matrix = pygdml.transformation.axisangle2matrix(axis, angle)
+    rotation_matrix = trf.axisangle2matrix(axis, angle)
     # converts a point in the fluka coordinate system to its point in
     # the bdsim global coordinate system.
     fluka_point_in_bdsim = fluka_point - fluka_bounding_box_origin
     rotated_point = rotation_matrix.dot(fluka_point_in_bdsim).A1
     offset = bdsim_point - rotated_point
     return offset
-
-def get_placement_string(output_name,
-                         gdml_path,
-                         bdsim_point,
-                         fluka_point,
-                         fluka_bounding_box_origin,
-                         bdsim_rotation,
-                         fluka_direction):
-    """
-    output_name -- the name of the output geometry placement.
-    gdml_path -- the path of the GDML file.
-    bdsim_point -- the point in BDSIM-world to coincide with
-                         `fluka_point` in BDSIM-world.
-    fluka_point -- the point in FLUKA-world to coincide with
-                         `bdsim_point` in BDSIM-world
-    fluka_bounding_box_origin -- the centre of the bounding box in
-                         FLUKA-world.  This coordinate is in the
-                         dictionary returned by
-                         pyfluka.Model.write_to_gdml.
-    bdsim_rotation -- the rotation that fluka_direction will be
-                         aligned with.  You will probably want to
-                         align your geometry w.r.t a specific
-                         component, in which case you will find it easiest
-                         this from the BDSOutputROOTEventModel tree of
-                         the rootevent output for your beamline.
-    fluka_direction -- the vector that will be used to align the
-                         geometry parallel to the rotation
-                         `bdsim_rotation` in BDSIM-world.
-
-    """
-    rotation = _get_bdsim_rotation(fluka_direction, bdsim_rotation)
-    placement = _get_bdsim_placement(bdsim_point, fluka_point,
-                                     fluka_bounding_box_origin,
-                                     rotation.axis, rotation.angle)
-    return _build_placement_string(output_name, gdml_path, placement,
-                                   rotation.axis, rotation.angle)
-
 
 def _build_placement_string(name, filepath, offset, axis, angle):
     """All units should be in millimetres."""
