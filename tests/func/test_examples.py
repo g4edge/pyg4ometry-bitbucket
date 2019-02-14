59e1362921c5a1254e00c0944d511f43c7b58e02
swalker92 <zwap082@live.rhul.ac.uk>
2019-02-14 13:23:14 +0000

    additional examples with tests

--- tests/func/test_examples.py
+++ tests/func/test_examples.py
@@ -10,3 +10,72 @@ def test_example_box_from_xzp_xyp_yzp():
     assert extent.centre.x == 3
     assert extent.centre.y == 6
     assert extent.centre.z == 9
+
+def test_finite_cylinder_parallel_to_x_axis():
+    xcentre = 2
+    ycentre = 10
+    zcentre = 10
+    length = 20
+    radius = 4
+
+    cylinder_zone = ex.finite_cylinder_parallel_to_x_axis(xcentre=xcentre,
+                                                          ycentre=ycentre,
+                                                          zcentre=zcentre,
+                                                          length=length,
+                                                          radius=radius)
+    extent = cylinder_zone.extent()
+    assert extent.lower.x == xcentre - length / 2.0
+    assert extent.upper.x == xcentre + length / 2.0
+
+    assert extent.lower.y == ycentre - radius
+    assert extent.upper.y == ycentre + radius
+
+    assert extent.lower.z == zcentre - radius
+    assert extent.upper.z == zcentre + radius
+
+def test_finite_cylinder_parallel_to_y_axis():
+    xcentre = 2
+    ycentre = 10
+    zcentre = 10
+    length = 20
+    radius = 4
+
+    cylinder_zone = ex.finite_cylinder_parallel_to_y_axis(xcentre=xcentre,
+                                                          ycentre=ycentre,
+                                                          zcentre=zcentre,
+                                                          length=length,
+                                                          radius=radius)
+    extent = cylinder_zone.extent()
+
+    assert extent.lower.x == xcentre - radius
+    assert extent.upper.x == xcentre + radius
+
+    assert extent.lower.y == ycentre - length / 2.0
+    assert extent.upper.y == ycentre + length / 2.0
+
+    assert extent.lower.z == zcentre - radius
+    assert extent.upper.z == zcentre + radius
+
+
+def test_finite_cylinder_parallel_to_z_axis():
+    xcentre = 2
+    ycentre = 10
+    zcentre = 10
+    length = 20
+    radius = 4
+
+    cylinder_zone = ex.finite_cylinder_parallel_to_z_axis(xcentre=xcentre,
+                                                          ycentre=ycentre,
+                                                          zcentre=zcentre,
+                                                          length=length,
+                                                          radius=radius)
+    extent = cylinder_zone.extent()
+
+    assert extent.lower.x == xcentre - radius
+    assert extent.upper.x == xcentre + radius
+
+    assert extent.lower.y == ycentre - radius
+    assert extent.upper.y == ycentre + radius
+
+    assert extent.lower.z == zcentre - length / 2.0
+    assert extent.upper.z == zcentre + length / 2.0
