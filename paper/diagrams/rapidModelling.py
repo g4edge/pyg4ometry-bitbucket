ec368f7ea47f6632133f6a46ab81f5b90e8ae453
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-02-20 19:11:54 +0000

    Added example

--- paper/diagrams/rapidModelling.py
+++ paper/diagrams/rapidModelling.py
@@ -6,9 +6,9 @@ import pyg4ometry.visualisation as vi
 reg = g4.Registry()
 
 # expressions
-wx = gd.Constant("wx","100",reg)
-wy = gd.Constant("wy","100",reg)
-wz = gd.Constant("wz","100",reg)
+wx = gd.Constant("wx","50",reg)
+wy = gd.Constant("wy","50",reg)
+wz = gd.Constant("wz","50",reg)
 bx = gd.Constant("bx","10",reg)
 by = gd.Constant("by","10",reg)
 bz = gd.Constant("bz","10",reg)
@@ -25,11 +25,18 @@ b  = g4.solid.Box("b",bx,by,bz,reg)
 wl = g4.LogicalVolume(wb, wm, "wl", reg)
 bl = g4.LogicalVolume(b, bm, "b", reg)
 bp1 = g4.PhysicalVolume([0,0,0],[0,0,0],
-                        bl, "b_pv1", wl, [1,1,1], reg)
-bp2 = g4.PhysicalVolume([0,0,0],[-2*bx,0,0],
-                        bl, "b_pv2", wl, [1,1,1], reg)
-bp3 = g4.PhysicalVolume([0,0,0],[2*bx,0,0],
-                        bl, "b_pv3", wl, [1,1,1], reg)
+                        bl, "b_pv1", wl, reg)
+bp2 = g4.PhysicalVolume([0,0,-0.25],[-3*bx,0,0],
+                        bl, "b_pv2", wl, reg)
+bp3 = g4.PhysicalVolume([0,0,0.5],[3*bx,0,0],
+                        bl, "b_pv3", wl, reg)
+
+# physical volume vistualisation attributes
+bp1.visOptions.color = (1,0,0)
+bp1.visOptions.alpha = 0.25
+bp2.visOptions.color = (0,1,0)
+bp2.visOptions.representation = "wireframe"
+bp3.visOptions.color = (0,0,1)
 
 # gdml output
 w = gd.Writer()
@@ -38,4 +45,5 @@ w.write(reg,"output.gdml")
 # visualisation
 v = vi.VtkViewer()
 v.addLogicalVolume(wl)
+v.addAxes()
 v.view()
