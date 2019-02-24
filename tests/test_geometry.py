35b3d4a9c82e506ea1ca394071ee1bda020c4f25
swalker92 <zwap082@live.rhul.ac.uk>
2019-02-24 14:44:26 +0000

    wrote a test for subtracting from world volume

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -13,6 +13,26 @@ def RPP():
 def wv():
     return geo.make_world_volume("worldname", "material")
 
+def test_subtract_from_world_volume(wv):
+    # cube with sides of length 1 and centeres at the origin.
+    cube = geo.RPP("cube", [-0.5, -0.5, -0.5], [0.5, 0.5, 0.5])
+    # rcc which goes through the centre of cube, centred on (x, y) = (0, 0)
+    # and pointing along z, radius 0.25.
+    hole = geo.RCC("hole", [0., 0., -1.], [0., 0.,1.], 0.25)
+    zone = geo.Zone([("+", cube), ("-", hole)])
+    region = geo.Region("r", [zone], "a material")
+    region.add_to_volume(wv)
+
+    geo.subtract_from_world_volume(wv, [hole])
+
+    subtraction = pygdml.solid.Subtraction("name",
+                                           cube.gdml_solid(),
+                                           hole.gdml_solid(length_safety="trim"),
+                                           [[3.141592653589793,
+                                             0.0,
+                                             3.141592653589793],
+                                            [ 0. ,  0. , -0.5]])
+    assert pygdml.solid.equal_tree(wv.currentVolume, subtraction)
 
 def test_make_world_volume_result_material(wv):
     assert wv.material == "material"
