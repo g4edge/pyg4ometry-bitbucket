dc2ba7dafc9df1d15553d0a9eae1f96a36172ccc
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-05-11 20:11:28 +0100

    Updated tests

--- pyg4ometry/test/python/PythonAuthoringTests.py
+++ pyg4ometry/test/python/PythonAuthoringTests.py
@@ -3,6 +3,10 @@ import unittest as _unittest
 import T001_Box
 import T002_Tubs
 import T003_CutTubs
+import T004_Cons
+import T005_Para
+import T006_Trd
+import T007_Trap
 
 import logging as _log
 
@@ -18,6 +22,18 @@ class PythonAuthoringTests(_unittest.TestCase) :
 
     def testCutTubs(self) :
         self.assertTrue(T003_CutTubs.Test())
+
+    def testCons(self) :
+        self.assertTrue(T004_Cons.Test())
+
+    def testPara(self) :
+        self.assertTrue(T005_Para.Test())
+
+    def testTrd(self) :
+        self.assertTrue(T006_Trd.Test())
+
+    def testTrap(self) :
+        self.assertTrue(T007_Trap.Test())
 
 if __name__ == '__main__':
     _unittest.main(verbosity=2)        
