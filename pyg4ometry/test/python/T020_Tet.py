5f2415947a784b7b60028a54faee2fd1db3aa267
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-05-12 16:05:43 +0100

    Tet test

---
+++ pyg4ometry/test/python/T020_Tet.py
@@ -0,0 +1,46 @@
+import os as _os
+import pyg4ometry.gdml as _gd
+import pyg4ometry.geant4 as _g4
+import pyg4ometry.visualisation as _vi
+
+
+def Test(vis = False) : 
+    reg = _g4.Registry()
+    
+    # defines 
+    wx = _gd.Constant("wx","100",reg,True)
+    wy = _gd.Constant("wy","100",reg,True)
+    wz = _gd.Constant("wz","100",reg,True)
+    
+    v1 = _gd.Position("v1","10","10","0","mm",reg,True)
+    v2 = _gd.Position("v2","-10","10","0","mm",reg,True)
+    v3 = _gd.Position("v3","-10","-10","0","mm",reg,True)
+    v4 = _gd.Position("v4","0","0","10","mm",reg,True)
+    
+    wm = _g4.MaterialPredefined("G4_Galactic") 
+    tm = _g4.MaterialPredefined("G4_Fe") 
+
+    # solids
+    ws = _g4.solid.Box("ws",wx,wy,wz, reg, "mm")
+    ts = _g4.solid.Tet("ts", v1, v2, v3, v4, reg)
+        
+    # structure 
+    wl = _g4.LogicalVolume(ws, wm, "wl", reg)
+    tl = _g4.LogicalVolume(ts, tm, "tl", reg)
+    tp = _g4.PhysicalVolume([0,0,0],[0,0,0],  tl, "t_pv1", wl, reg) 
+    
+    # set world volume
+    reg.setWorld(wl.name)
+    
+    # gdml output 
+    w = _gd.Writer()
+    w.addDetector(reg)
+    w.write(_os.path.join(_os.path.dirname(__file__), "T020_Tet.gdml"))
+
+    # visualisation
+    if vis : 
+        v = _vi.VtkViewer()
+        v.addLogicalVolume(reg.getWorldVolume())
+        v.view()
+
+    return True
