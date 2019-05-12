8222b92d6f527e9e24b3b6f0e7a1b41007427e2f
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-05-12 15:11:30 +0100

    Hyperboloid test

--- pyg4ometry/test/python/T019_Hyperboloid.py
+++ pyg4ometry/test/python/T019_Hyperboloid.py
@@ -12,22 +12,24 @@ def Test(vis = False) :
     wy = _gd.Constant("wy","100",reg,True)
     wz = _gd.Constant("wz","100",reg,True)
     
-    pi   = _gd.Constant("pi","3.1415926",reg,True)
-    prlo = _gd.Constant("prlo","2",reg,True)
-    prhi = _gd.Constant("prhi","15",reg,True)
-    pz   = _gd.Constant("pz","50",reg,True)
+    pi    = _gd.Constant("pi","3.1415926",reg,True)
+    hrmin = _gd.Constant("hrmin","1.0",reg,True)
+    hrmax = _gd.Constant("hrmax","2.0",reg,True)
+    hz    = _gd.Constant("hz","20.0",reg,True)
+    hinst = _gd.Constant("hinst","3",reg,True)
+    houtst= _gd.Constant("houtst","4",reg,True)
 
     wm = _g4.MaterialPredefined("G4_Galactic") 
-    pm = _g4.MaterialPredefined("G4_Fe") 
+    hm = _g4.MaterialPredefined("G4_Fe") 
 
     # solids
     ws = _g4.solid.Box("ws",wx,wy,wz, reg, "mm")
-    ps = _g4.solid.Paraboloid("ps",pz,prlo,prhi,reg)
+    hs = _g4.solid.Hype("ps",hrmin, hrmax, hinst, houtst, hz, reg)
         
     # structure 
     wl = _g4.LogicalVolume(ws, wm, "wl", reg)
-    pl = _g4.LogicalVolume(ps, pm, "pl", reg)
-    pp = _g4.PhysicalVolume([0,0,0],[0,0,0],  pl, "p_pv1", wl, reg) 
+    hl = _g4.LogicalVolume(hs, hm, "hl", reg)
+    hp = _g4.PhysicalVolume([0,0,0],[0,0,0],  hl, "h_pv1", wl, reg) 
     
     # set world volume
     reg.setWorld(wl.name)
@@ -35,7 +37,7 @@ def Test(vis = False) :
     # gdml output 
     w = _gd.Writer()
     w.addDetector(reg)
-    w.write(_os.path.join(_os.path.dirname(__file__), "T018_Paraboloid.gdml"))
+    w.write(_os.path.join(_os.path.dirname(__file__), "T019_Hyperboloid.gdml"))
 
     # visualisation
     if vis : 
