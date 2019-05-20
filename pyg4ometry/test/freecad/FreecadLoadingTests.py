5c84c736a9366c5c25b90dc0e8c5eade16069a4d
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-05-20 10:40:00 +0100

    Freecad tests

---
+++ pyg4ometry/test/freecad/FreecadLoadingTests.py
@@ -0,0 +1,42 @@
+import unittest as _unittest
+
+import pyg4ometry.geant4 as _g4
+import pyg4ometry.freecad as _fc
+
+import logging as _log
+import os as _os
+
+logger = _log.getLogger()
+logger.disabled = True
+
+def LoadFreecad(fileName) : 
+    r = _fc.Reader(fileName) 
+    r.convertFlat()
+    
+    return True
+
+def _pj(filename):
+    """
+    Append the absolute path to *this* directory to the filename so the tests
+    can be ran from anywhere
+    """
+    return _os.path.join(_os.path.dirname(__file__), filename)
+
+class FreecadLoadingTests(_unittest.TestCase) :
+    def testFreecadLoadBasicSolids(self) :
+        self.assertTrue(LoadFreecad(_pj("01_BasicSolids.step")))
+
+    def testFreecadLoadBooleanSolids(self) :
+        self.assertTrue(LoadFreecad(_pj("02_BooleanSolids.step")))
+
+    def testFreecadLoadSketchPadSolids(self) :
+        self.assertTrue(LoadFreecad(_pj("03_SketchPad.step")))
+
+    def testFreecadLoadSketchPadSolids(self) :
+        self.assertTrue(LoadFreecad(_pj("04_Rubik.step")))
+
+    def testFreecadLoadPlacement(self) :
+        self.assertTrue(LoadFreecad(_pj("05_Placement.step")))
+            
+if __name__ == '__main__':
+    _unittest.main(verbosity=2)        
--- pyg4ometry/test/runTests.py
+++ pyg4ometry/test/runTests.py
@@ -1,6 +1,8 @@
-from python import PythonAuthoringTests, PythonDefineTests
-from gdml   import GdmlLoadTests
-from stl    import StlLoadingTests
+from freecad import FreecadLoadingTests
+from python  import PythonDefineTests
+from python  import PythonAuthoringTests
+from gdml    import GdmlLoadTests
+from stl     import StlLoadingTests
 
 import unittest as _unittest
 import logging as _log
@@ -10,6 +12,3 @@ logger.disabled = True
 
 if __name__ == '__main__':
     _unittest.main(verbosity=2)
-
-
-
