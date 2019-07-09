0cfe4d4fedf9672d4024e1ac2a2b497fa59f47a9
swalker92 <zwap082@live.rhul.ac.uk>
2019-07-09 15:40:01 +0100

    work in progress as i move to the Reader/Registry style for fluka
    refactoring to fit the Reader/Registry approach from the old style.
    doesn't work, fixing various errors.

---
+++ pyg4ometry/test/fluka/sphere.py
@@ -0,0 +1,16 @@
+import pyg4ometry.geant4 as g4
+import pyg4ometry.fluka as fluka
+
+import pyg4ometry
+
+
+def test_read(vis=False):
+    reader = fluka.Reader("sphere.inp")
+
+
+def test_write(vis=False):
+
+
+
+if __name__ == '__main__':
+    test(vis=True)
