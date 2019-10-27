440250501ef8d6713e1a9c07142d06577de15a62
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-10-27 09:50:17 +0000

    Add timing to runCoverage

--- coverage/runCoverage.sh
+++ coverage/runCoverage.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+
+# 1150.988s 2019/10/27
+
 coverage-2.7 run --source pyg4ometry --omit="*fluka*","*test*","*pygeometry*" ../pyg4ometry/test/runTests.py
 coverage-2.7 report -m > runCoverage.log
