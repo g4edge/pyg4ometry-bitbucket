4b0bacf5bcd1e942c6fda12fad501ac0bc9cc76e
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-11-17 14:41:01 +0000

    Test without coplanar faces

--- pyg4ometry/test/pythonGeant4/T104_overlap_volu.py
+++ pyg4ometry/test/pythonGeant4/T104_overlap_volu.py
@@ -24,18 +24,24 @@ def Test(vis = False, interactive = False) :
 
     # solids
     ws = _g4.solid.Box("ws",wx,wy,wz, reg, "mm")
-    bs = _g4.solid.Box("bs",1.1*bx,0.9*bx,0.9*bx, reg, "mm")
+    bs1 = _g4.solid.Box("bs1",1.1*bx,0.9*bx,0.9*bx, reg, "mm")
+    bs2 = _g4.solid.Box("bs2",1.1*bx,0.8*bx,0.8*bx, reg, "mm")
+    bs3 = _g4.solid.Box("bs3",1.1*bx,0.7*bx,0.7*bx, reg, "mm")
+
     rs = _g4.solid.Box("rs",3*bx,2*bx,2*bx, reg, "mm")
 
     # structure 
     wl = _g4.LogicalVolume(ws, wm, "wl", reg)
 
-    bl = _g4.LogicalVolume(bs, bm, "bl", reg)
+    bl1 = _g4.LogicalVolume(bs1, bm, "bl1", reg)
+    bl2 = _g4.LogicalVolume(bs2, bm, "bl2", reg)
+    bl3 = _g4.LogicalVolume(bs3, bm, "bl3", reg)
+
     rl = _g4.LogicalVolume(rs, wm, "rl", reg)
 
-    bp1 = _g4.PhysicalVolume([0.0,0,0],  [-bx,0,0],  bl, "b_pv1", rl, reg)
-    bp2 = _g4.PhysicalVolume([0.0,0,0],  [0  ,0,0],  bl, "b_pv2", rl, reg)
-    bp3 = _g4.PhysicalVolume([0.0,0,0],[bx ,0,0],  bl, "b_pv3", rl, reg)
+    bp1 = _g4.PhysicalVolume([0.0,0,0],  [-bx,0,0],  bl1, "b_pv1", rl, reg)
+    bp2 = _g4.PhysicalVolume([0.0,0,0],  [0  ,0,0],  bl2, "b_pv2", rl, reg)
+    bp3 = _g4.PhysicalVolume([0.0,0,0],  [bx ,0,0],  bl3, "b_pv3", rl, reg)
 
     rp  = _g4.PhysicalVolume([0,0,0],  [0,0,0],    rl, "r_pv1", wl, reg)
 
@@ -51,7 +57,7 @@ def Test(vis = False, interactive = False) :
     w.write(_os.path.join(_os.path.dirname(__file__), "T104_overlap_volu.gdml"))
 
     # test __repr__
-    str(bs)
+    str(bs1)
 
     # test extent of physical volume
     wlextent           = wl.extent(True)
@@ -63,7 +69,7 @@ def Test(vis = False, interactive = False) :
         v.addLogicalVolume(reg.getWorldVolume())
         v.view(interactive=interactive)
 
-    return {"testStatus": True, "logicalVolume":wl}
+    return {"testStatus": True, "logicalVolume":wl, "vtkViewer":v}
 
 if __name__ == "__main__":
     Test()
