58fac0622e8f50698bb2fc4a4aaa517ee7807808
swalker92 <zwap082@live.rhul.ac.uk>
2019-11-18 17:32:54 +0000

    New bodies, convert FLUKAregistry to G4registry, Boolean fixes
    fail more clearly when giving wrong type of argument
    
    Added SPH, RCC, TRC, bodies and beginnings of (simple) half spaces
    
    simple in the sense that their infinite extent is currently hardcoded
    to be a fairly large number, which is fine for now.
    
    inherit object in python2 classes
    
    np.matrix -> np.array
    
    np.matrix has been deprecated, just use arrays.  external
    functionality should be identical.
    
    added test for box from 6 half spaces (XYP, XZP, YZP)
    
    Booleans didn't work but I think they now probably do
    
    transformations were completely wrong.
    
    also trying to make a union would just crash and be wrong i think..
    
    fix AttributeError when calling addRegion
    
    useful method for printing contents of FlukaRegistry
    
    FlukaRegistry to G4Registry method
    
    inherit object for base classes in python2

---
+++ pyg4ometry/test/pythonFluka/T005_XYP_XZP_YZP.py
@@ -0,0 +1,59 @@
+import pyg4ometry.geant4 as g4
+from pyg4ometry.fluka.Body import XYP, XZP, YZP
+from pyg4ometry.fluka.Region import Region, Zone
+from pyg4ometry.fluka.FlukaRegistry import FlukaRegistry
+import pyg4ometry.visualisation as vi
+
+
+def Test(vis=False, interactive=False):
+    freg = FlukaRegistry()
+    greg = g4.Registry()
+    # I pick 20 because that's the length of the axes added below, so
+    # verifying the resulting cube is of the correct length is trivial.
+    xyp_lo = XYP("XYP1_BODY", 0, flukaregistry=freg)
+    xyp_hi = XYP("XYP2_BODY", 20.0, flukaregistry=freg)
+
+    xzp_lo = XZP("XZP1_BODY", 0, flukaregistry=freg)
+    xzp_hi = XZP("XZP2_BODY", 20.0, flukaregistry=freg)
+
+    yzp_lo = YZP("YZP1_BODY", 0, flukaregistry=freg)
+    yzp_hi = YZP("YZP2_BODY", 20.0, flukaregistry=freg)
+
+
+
+    z = Zone()
+
+    z.addIntersection(xyp_hi)
+    z.addSubtraction(xyp_lo)
+
+    z.addIntersection(xzp_hi)
+    z.addSubtraction(xzp_lo)
+
+    z.addIntersection(yzp_hi)
+    z.addSubtraction(yzp_lo)
+
+
+    region = Region("REG_INF")
+    region.addZone(z)
+
+    freg.addRegion(region)
+
+    greg = freg.toG4Registry()
+
+
+    # Test extents??
+    # clip wv?
+    # test writing back to fluka?
+
+    if vis:
+        v = vi.VtkViewer()
+        v.addAxes(length=20)
+        v.addLogicalVolume(greg.getWorldVolume())
+        v.view(interactive=interactive)
+
+    return {"testStatus": True, "logicalVolume": greg.getWorldVolume()}
+
+
+
+if __name__ == '__main__':
+    Test(True, True)
