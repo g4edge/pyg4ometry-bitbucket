1e06a776e3bd57affbcfd978bb6bcee671e7e20f
swalker92 <zwap082@live.rhul.ac.uk>
2019-11-26 15:19:14 +0000

    Added test for nested subtraction: +A -(+B -C).  It works.

---
+++ pyg4ometry/test/pythonFluka/nested_subtraction.py
@@ -0,0 +1,52 @@
+import pyg4ometry.geant4 as g4
+from pyg4ometry.fluka.Body import RPP, ZCC
+from pyg4ometry.fluka.Region import Region, Zone
+from pyg4ometry.fluka.FlukaRegistry import FlukaRegistry
+import pyg4ometry.visualisation as vi
+
+
+def Test(vis=False, interactive=False):
+    freg = FlukaRegistry()
+    greg = g4.Registry()
+
+    rpp = RPP("RPP_BODY", -20, 20, -20, 20, -20, 20, flukaregistry=freg)
+    rppsub = RPP("RPP2_BODY", -10, 10, -10, 10, -30, 30, flukaregistry=freg)
+
+    zcc = ZCC("ZCC_BODY", 0, 0, 10, flukaregistry=freg)
+
+    z = Zone()
+    z.addIntersection(rpp)
+
+    z2 = Zone()
+    z2.addIntersection(rppsub)
+    z2.addSubtraction(zcc)
+
+    # Adding zone2 as a subtraction to the first zone.
+    z.addSubtraction(z2)
+
+    region = Region("RPP_REG")
+
+    region.addZone(z)
+    # region.addZone(z2)
+
+    freg.addRegion(region)
+
+    greg = freg.toG4Registry()
+
+    # from IPython import embed; embed()
+
+    # Test extents??
+    # clip wv?
+
+    if vis:
+        v = vi.VtkViewer()
+        v.addAxes(length=20)
+        v.addLogicalVolume(greg.getWorldVolume())
+        v.view(interactive=interactive)
+
+    return {"testStatus": True, "logicalVolume": greg.getWorldVolume()}
+
+
+
+if __name__ == '__main__':
+    Test(True,True)
