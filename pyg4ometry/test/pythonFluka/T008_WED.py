a543308aae5ed7c71913b05d3cf5a2a8feaaca3e
swalker92 <zwap082@live.rhul.ac.uk>
2019-11-26 10:50:32 +0000

    finally got WED/RAW working as I wanted them to
    improved test, added a __repr__
    
    updated RAW test

--- pyg4ometry/test/pythonFluka/T008_WED.py
+++ pyg4ometry/test/pythonFluka/T008_WED.py
@@ -10,37 +10,34 @@ def Test(vis=False, interactive=False):
     freg = FlukaRegistry()
     greg = g4.Registry()
 
-    vertex = [0, 0, 0]
-    direction = Three([3, 3, 3])
-    side = Three([-0.5, 1, -0.5])
-    other_side = np.cross(side, direction)
+    # What I expect to see in the visualiser is a cube formed by the
+    # union of two wedeges. with sides equal to 20cm.  The mesh shows
+    # the two wedges.
 
-    side_length = side.length()
-
-    other_side = 1 * (side_length * other_side /
-                     np.linalg.norm(other_side)) # 2x length of other side
-
-    # other_side = [0.5, -1, 0.5]
-
-
-#     wed = WED("WED_BODY", [0, 0, 0],
-# # [10, 10, 10],
-#               [10, 0, 0], # one transverse side.
-#               [10, 10, 10], # length vector.
-#               [0, 10, 0], # the other transverse side.
-#               freg)
-
-    wed = WED("WED_BODY",
-              vertex,
-              side, # one transverse side.
-              direction, # length vector.
-              other_side, # the other transverse side.
+    wed1 = WED("WED1_BODY",
+              [20, 20, 20], # vertex position
+              [-20, 0, 0], # one transverse side.
+              [0, 0, -20], # length vector.
+              [0, -20, 0], # the other transverse side.
               freg)
 
-    z = Zone()
-    z.addIntersection(wed)
+    wed2 = WED("WED2_BODY",
+               [0, 0, 0],
+               [20, 0, 0], # one transverse side.
+               [0, 0, 20], # length vector.
+               [0, 20, 0], # the other transverse side.
+               freg)
+
+    
+    z1 = Zone()
+    z1.addIntersection(wed1)
+
+    z2 = Zone()
+    z2.addIntersection(wed2)
+
     region = Region("WED_REG")
-    region.addZone(z)
+    region.addZone(z1)
+    region.addZone(z2)
     freg.addRegion(region)
 
     greg = freg.toG4Registry()
