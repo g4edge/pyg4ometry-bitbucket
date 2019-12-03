3843264b6d194dc2699689dcf0d8b16e1afda537
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-03 14:17:14 +0000

    geometry directive translation test

---
+++ pyg4ometry/test/pythonFluka/T052_translation.py
@@ -0,0 +1,39 @@
+import pyg4ometry.geant4 as g4
+from pyg4ometry.fluka.Body import RPP
+from pyg4ometry.fluka.Region import Region, Zone
+from pyg4ometry.fluka.FlukaRegistry import FlukaRegistry
+import pyg4ometry.visualisation as vi
+
+
+def Test(vis=False, interactive=False):
+    freg = FlukaRegistry()
+    greg = g4.Registry()
+
+    translation = [-10, -10, -10]
+    # translation = [-0, -0, -0]
+
+    rpp = RPP("RPP_BODY", 10, 20, 10, 20, 10, 20, flukaregistry=freg,
+              translation=translation)
+    z = Zone()
+    z.addIntersection(rpp)
+    region = Region("RPP_REG")
+    region.addZone(z)
+    freg.addRegion(region)
+
+    greg = freg.toG4Registry()
+
+    # Test extents??
+    # clip wv?
+
+    if vis:
+        v = vi.VtkViewer()
+        v.addAxes(length=20)
+        v.addLogicalVolume(greg.getWorldVolume())
+        v.view(interactive=interactive)
+
+    return {"testStatus": True, "logicalVolume": greg.getWorldVolume()}
+
+
+
+if __name__ == '__main__':
+    Test(True, True)
