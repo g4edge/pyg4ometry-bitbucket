6d7ca799cf313107a117d95f4d9c92542ad12979
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-03 14:40:23 +0000

    expansion test case

---
+++ pyg4ometry/test/pythonFluka/T051_expansion.py
@@ -0,0 +1,38 @@
+import pyg4ometry.geant4 as g4
+from pyg4ometry.fluka.Body import RPP
+from pyg4ometry.fluka.Region import Region, Zone
+from pyg4ometry.fluka.FlukaRegistry import FlukaRegistry
+import pyg4ometry.visualisation as vi
+
+
+def Test(vis=False, interactive=False):
+    freg = FlukaRegistry()
+    greg = g4.Registry()
+
+    expansion = 2
+
+    rpp = RPP("RPP_BODY", 0, 10, 0, 10, 0, 10, flukaregistry=freg,
+              expansion=expansion)
+    z = Zone()
+    z.addIntersection(rpp)
+    region = Region("RPP_REG")
+    region.addZone(z)
+    freg.addRegion(region)
+
+    greg = freg.toG4Registry()
+
+    # Test extents??
+    # clip wv?
+
+    if vis:
+        v = vi.VtkViewer()
+        v.addAxes(length=20)
+        v.addLogicalVolume(greg.getWorldVolume())
+        v.view(interactive=interactive)
+
+    return {"testStatus": True, "logicalVolume": greg.getWorldVolume()}
+
+
+
+if __name__ == '__main__':
+    Test(True, True)
