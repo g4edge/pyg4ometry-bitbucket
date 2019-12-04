e7e13656ecdbcbda980634c915597d6d71c3b74e
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-04 11:36:30 +0000

    better RPP coplanarity test

--- pyg4ometry/test/pythonFluka/T201_RPP_coplanar.py
+++ pyg4ometry/test/pythonFluka/T201_RPP_coplanar.py
@@ -1,60 +1,52 @@
-import os
 import pyg4ometry.geant4 as g4
 from pyg4ometry.fluka.Body import RPP
 from pyg4ometry.fluka.Region import Region, Zone
 from pyg4ometry.fluka.FlukaRegistry import FlukaRegistry
 import pyg4ometry.visualisation as vi
-import pyg4ometry.gdml as gdml
 
 
 def Test(vis=False, interactive=False):
     freg = FlukaRegistry()
     greg = g4.Registry()
 
-    # trivially coplanar:
-    rpp1 = RPP("RPP1_BODY", 0, 10, 0, 10, 0, 10, flukaregistry=freg)
-    rpp2 = RPP("RPP2_BODY", 10, 20, 0, 10, 0, 10, flukaregistry=freg)    
+    rpp1 = RPP("RPP_BODY1", 0, 10, 0, 10, 0, 10, flukaregistry=freg)
+    rpp2 = RPP("RPP_BODY2", 2, 8, 2, 8, 2, 8, flukaregistry=freg)
 
     z1 = Zone()
     z2 = Zone()
 
     z1.addIntersection(rpp1)
-    z2.addIntersection(rpp2)    
+    z1.addSubtraction(rpp2)
+
+    z2.addIntersection(rpp2)
 
     region1 = Region("RPP_REG1")
-    region2 = Region("RPP_REG2")    
+    region2 = Region("RPP_REG2")
 
     region1.addZone(z1)
     region2.addZone(z2)
 
     freg.addRegion(region1)
-    freg.addRegion(region2)    
+    freg.addRegion(region2)
 
-    
-    # default is True, but to be explicit:
-    greg = freg.toG4Registry(with_length_safety=True) 
+    greg = freg.toG4Registry(with_length_safety=True)
 
-    wv = greg.getWorldVolume()
-    wv.checkOverlaps()
+
+    wlv = greg.getWorldVolume()
+    wlv.checkOverlaps()
 
     # Test extents??
     # clip wv?
-    
+
     if vis:
         v = vi.VtkViewer()
         v.addAxes(length=20)
-        v.addLogicalVolume(wv)
+        v.addLogicalVolume(wlv)
         v.view(interactive=interactive)
 
-        
     return {"testStatus": True, "logicalVolume": greg.getWorldVolume()}
-        
+
 
 
 if __name__ == '__main__':
     Test(True, True)
-
-    
-
-
-    
