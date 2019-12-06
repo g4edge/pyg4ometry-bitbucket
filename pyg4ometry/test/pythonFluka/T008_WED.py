8215f190acf1825431acaf9eb7a6a9fcc2d8bd54
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-06 13:42:59 +0000

    fix broken WED/RAW tests

--- pyg4ometry/test/pythonFluka/T008_WED.py
+++ pyg4ometry/test/pythonFluka/T008_WED.py
@@ -19,16 +19,15 @@ def Test(vis=False, interactive=False):
               [-20, 0, 0], # one transverse side.
               [0, 0, -20], # length vector.
               [0, -20, 0], # the other transverse side.
-              freg)
+               flukaregistry=freg)
 
     wed2 = WED("WED2_BODY",
                [0, 0, 0],
                [20, 0, 0], # one transverse side.
                [0, 0, 20], # length vector.
                [0, 20, 0], # the other transverse side.
-               freg)
+               flukaregistry=freg)
 
-    
     z1 = Zone()
     z1.addIntersection(wed1)
 
