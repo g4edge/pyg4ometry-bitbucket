afe8257bca55e389bb6602ad763f17e5f1072bcb
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-06 17:09:43 +0000

    PLA test

---
+++ pyg4ometry/test/pythonFluka/T010_PLA.py
@@ -0,0 +1,45 @@
+import pyg4ometry.geant4 as g4
+from pyg4ometry.fluka.Body import PLA
+from pyg4ometry.fluka.Region import Region, Zone
+from pyg4ometry.fluka.FlukaRegistry import FlukaRegistry
+import pyg4ometry.visualisation as vi
+
+
+def Test(vis=False, interactive=False):
+    freg = FlukaRegistry()
+    greg = g4.Registry()
+
+    pla1 = PLA("PLA1_BODY",
+               [1, 1, 1],
+               [0, 0.0, 0],
+               flukaregistry=freg)
+
+    z1 = Zone()
+
+
+    z1.addIntersection(pla1)
+
+    region = Region("REG_INF")
+    region.addZone(z1)
+
+    freg.addRegion(region)
+
+    greg = freg.toG4Registry()
+
+
+    # Test extents??
+    # clip wv?
+    # test writing back to fluka?
+
+    if vis:
+        v = vi.VtkViewer()
+        v.addAxes(length=20)
+        v.addLogicalVolume(greg.getWorldVolume())
+        v.view(interactive=interactive)
+
+    return {"testStatus": True, "logicalVolume": greg.getWorldVolume()}
+
+
+
+if __name__ == '__main__':
+    Test(True, True)
