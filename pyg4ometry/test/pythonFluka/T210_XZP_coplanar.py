cee898d1a1ad6b5c54b3503ce2ed55d5e68f18a2
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-06 13:12:06 +0000

    XYP XZP YZP automatic length safety tests.

---
+++ pyg4ometry/test/pythonFluka/T210_XZP_coplanar.py
@@ -0,0 +1,8 @@
+import T210_XYP_coplanar
+
+
+def Test(vis=False, interactive=False):
+    T210_XYP_coplanar.Test(vis=vis, interactive=interactive)
+
+if __name__ == '__main__':
+    Test(True, True)
