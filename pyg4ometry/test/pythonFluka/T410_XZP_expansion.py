5da9c47497d8bf77cc628aac9356e8c0b62f89cc
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-17 07:38:15 +0000

    Expansion code for many bodies with tests
    REC, XZP, XYP, YZP, X/Y/ZCC, X/Y/ZEC SPH, TRC, RCC, BOX, ARB, PLA,
    WED, RAW

---
+++ pyg4ometry/test/pythonFluka/T410_XZP_expansion.py
@@ -0,0 +1,36 @@
+import pyg4ometry.convert as convert
+import pyg4ometry.visualisation as vi
+from pyg4ometry.fluka import XZP, Region, Zone, FlukaRegistry
+import pyg4ometry.fluka.body
+
+def Test(vis=False, interactive=False):
+    freg = FlukaRegistry()
+
+    pyg4ometry.fluka.body.INFINITY = 30
+
+    xzp = XZP("XZP_BODY", 10.0,
+              expansion=2.0,
+              flukaregistry=freg)
+
+    z = Zone()
+    z.addIntersection(xzp)
+
+    region = Region("REG_INF")
+    region.addZone(z)
+
+    freg.addRegion(region)
+
+    greg = convert.fluka2Geant4(freg)
+
+    if vis:
+        v = vi.VtkViewer()
+        v.addAxes(length=20)
+        v.addLogicalVolume(greg.getWorldVolume())
+        v.view(interactive=interactive)
+
+    return {"testStatus": True, "logicalVolume": greg.getWorldVolume()}
+
+
+
+if __name__ == '__main__':
+    Test(True, True)
