6290e0066d0c16b595064c54dfe3893d34bd7b6f
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-19 16:22:51 +0000

    test_all.py file for loading all FLUKA input in the flairFluka dir

--- pyg4ometry/test/flairFluka/test_all.py
+++ pyg4ometry/test/flairFluka/test_all.py
@@ -5,10 +5,12 @@ import os
 import pyg4ometry.fluka as fluka
 import pyg4ometry.visualisation as vi
 import pyg4ometry.gdml as gdml
+from pyg4ometry.convert import fluka2Geant4
 
-def run_one(filein, vis=False, interactive=False):
+def run_one_test(filein, vis=False, interactive=False):
     r = fluka.Reader(filein)
-    greg = r.flukaregistry.toG4Registry()
+
+    greg = fluka2Geant4(r.flukaregistry)
 
     wlv = greg.getWorldVolume()
 
@@ -19,7 +21,6 @@ def run_one(filein, vis=False, interactive=False):
         v.addLogicalVolume(wlv)
         v.view(interactive)
 
-
     w = gdml.Writer()
     w.addDetector(greg)
     gdml_name = filein.rstrip(".inp") + ".gdml"
@@ -28,24 +29,16 @@ def run_one(filein, vis=False, interactive=False):
     w.writeGmadTester(gmad_name, gdml_name)
 
 
-def run_all(vis=False, interactive=False):
+def run_all_tests(vis=False, interactive=False):
     for f in glob.glob("*.inp"):
         print "---> Testing file = {}".format(f)
-        run_one(f, vis=vis, interactive=interactive)
-
-
-def main(f):
-    """ Either test an individual file, or if None is provided as the
-    argument, test all."""
-    if f is None:
-        run_all()
-    else:
-        run_one(f)
-
+        try:
+            run_one_test(f, vis=vis, interactive=interactive)
+        except Exception as e:
+            print "------> Test {} FAILED with message {}".format(f, e)
+            continue
 
 if __name__ == '__main__':
-    try:
-        main(sys.argv[1])
-    except IndexError:
-        main(None)
+    run_all_tests()
+
 
