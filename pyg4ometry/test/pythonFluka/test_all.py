208e7dfb52a6568f82de2e02feb443d97b8ee7a4
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-19 16:32:02 +0000

    better fail messages when running all python tests

--- pyg4ometry/test/pythonFluka/test_all.py
+++ pyg4ometry/test/pythonFluka/test_all.py
@@ -3,26 +3,12 @@ import os
 import sys
 import importlib
 
-# Skip modules with this string inside.  They are placeholder files
-# for future tests and will trivially fail, so don't test them.
-_PLACEHOLDER_STRINGS = ["QUA", "lattice"]
-
-def checkIfPlaceholders(module_name):
-    for s in _PLACEHOLDER_STRINGS:
-        if s.lower() in module_name.lower():
-            return True
-    return False
-
 def getAllTestModules():
 
     modules = []
     # for module in os.listdir(os.path.dirname(__file__)):
     for f in glob.glob("./*.py"):
         module_name = os.path.basename(f[:-3])
-
-        if checkIfPlaceholders(module_name):
-            print "Skipping placeholder test module", module_name
-            continue
 
         if module_name[0] != "T": # tests start with T here..
             continue
@@ -37,8 +23,8 @@ def main():
         try:
             result = module.Test()
         except Exception as e:
-            print "-------TEST FAILED IN: ", name
-            raise
+            print "------> Test {} FAILED with message {}".format(name, e)
+            continue
 
 
 if __name__ == '__main__':
