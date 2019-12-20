c5320cd9819ef2fff4eccb211d38b8e542b9e4e9
Ben Shellswell <zdap036@live.rhul.ac.uk>
2019-12-20 16:02:09 +0000

    accelerated meshing

--- pyg4ometry/test/pythonGeant4/T008_Sphere.py
+++ pyg4ometry/test/pythonGeant4/T008_Sphere.py
@@ -4,7 +4,7 @@ import pyg4ometry.geant4 as _g4
 import pyg4ometry.visualisation as _vi
 
 
-def Test(vis = False, interactive = False) :
+def Test(vis = False, interactive = False, n_slice=10, n_stack=10) :
     reg = _g4.Registry()
     
     # defines 
@@ -15,11 +15,11 @@ def Test(vis = False, interactive = False) :
     # pi     = _gd.Constant("pi","3.1415926",reg,True)
     srmin  = _gd.Constant("rmin","8",reg,True)    
     srmax  = _gd.Constant("rmax","10",reg,True)
-    ssphi  = _gd.Constant("sphi","0.0",reg,True)
+    ssphi  = _gd.Constant("sphi","0",reg,True)
     # sdphi  = _gd.Constant("dphi","2*pi",reg,True)
-    sdphi = _gd.Constant("dphi", "pi", reg, True)
-    sstheta= _gd.Constant("stheta","0.0",reg,True)
-    sdtheta= _gd.Constant("dtheta","3*pi/4",reg,True)
+    sdphi = _gd.Constant("dphi", "1.75*pi", reg, True)
+    sstheta= _gd.Constant("stheta","0",reg,True)
+    sdtheta= _gd.Constant("dtheta","0.75*pi",reg,True)
     # sdtheta = _gd.Constant("dtheta", "pi", reg, True)
 
     wm = _g4.MaterialPredefined("G4_Galactic") 
@@ -27,7 +27,7 @@ def Test(vis = False, interactive = False) :
 
     # solids
     ws = _g4.solid.Box("ws",wx,wy,wz, reg, "mm")
-    ss = _g4.solid.Sphere("ss",srmin,srmax,ssphi,sdphi,sstheta,sdtheta,reg,"mm","rad")
+    ss = _g4.solid.Sphere("ss",srmin,srmax,ssphi,sdphi,sstheta,sdtheta,reg,"mm","rad",nslice=n_slice, nstack=n_stack)
         
     # structure 
     wl = _g4.LogicalVolume(ws, wm, "wl", reg)
