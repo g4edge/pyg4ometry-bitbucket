30eb6c9e7f6372fbfd7bcc756cf3ead70a1d1d26
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-12-21 12:00:20 +0000

    Removed old pygeometry and added gui to omit list

--- coverage/runCoverage.sh
+++ coverage/runCoverage.sh
@@ -3,5 +3,5 @@
 
 # 1150.988s 2019/10/27
 
-coverage-2.7 run --source pyg4ometry --omit="*test*","*pygeometry*" ../pyg4ometry/test/runTests.py
+coverage-2.7 run --source pyg4ometry --omit="*test*","*gui*" ../pyg4ometry/test/runTests.py
 coverage-2.7 report -m > runCoverage.log
