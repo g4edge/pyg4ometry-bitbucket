e4a85997d6657c3da432f4d6b119fc61b0dceab2
swalker92 <zwap082@live.rhul.ac.uk>
2019-12-23 18:37:25 +0000

    minimisation for infinite elliptical cylinders + examples

---
+++ pyg4ometry/test/pythonFluka/T713_YEC_minimisation.py
@@ -0,0 +1,44 @@
+import pyg4ometry.convert as convert
+import pyg4ometry.visualisation as vi
+from pyg4ometry.fluka import YEC, XZP, Region, Zone, FlukaRegistry
+from pyg4ometry.fluka.body import INFINITY
+
+def Test(vis=False, interactive=False):
+    freg = FlukaRegistry()
+    # I pick 20 because that's the length of the axes added below, so
+    # verifying the resulting body is of the correct length and radius
+    # is trivial.
+    yec = YEC("YEC_BODY", 0, 0, 20, 10, flukaregistry=freg)
+
+    xzp_hi = XZP("XZP1_BODY", 20, flukaregistry=freg)
+    xzp_lo = XZP("XZP2_BODY", 0, flukaregistry=freg)
+
+    z = Zone()
+
+    z.addIntersection(yec)
+    z.addIntersection(xzp_hi)
+    z.addSubtraction(xzp_lo)
+
+
+    region = Region("REG_INF")
+    region.addZone(z)
+
+    freg.addRegion(region)
+
+    greg = convert.fluka2Geant4(freg,
+                                with_length_safety=True,
+                                split_disjoint_unions=False,
+                                minimise_solids=True)
+    print "Infinite cylinder length =", greg.solidDict["YEC_BODY_s"].pDz
+    assert greg.solidDict["YEC_BODY_s"].pDz < INFINITY
+
+    if vis:
+        v = vi.VtkViewer()
+        v.addAxes(length=20)
+        v.addLogicalVolume(greg.getWorldVolume())
+        v.view(interactive=interactive)
+
+    return {"testStatus": True, "logicalVolume": greg.getWorldVolume()}
+
+if __name__ == '__main__':
+    Test(True, True)
