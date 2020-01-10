f54a2d21eff0465e86cbdeb7661e1c9fa478349f
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2020-01-10 01:38:53 +0000

    Fixed transform fluka conversion

--- pyg4ometry/test/gdml2Fluka/T001_GdmlBox2Fluka.py
+++ pyg4ometry/test/gdml2Fluka/T001_GdmlBox2Fluka.py
@@ -2,6 +2,7 @@ import pyg4ometry.geant4 as _g4
 import pyg4ometry.convert as _convert
 import pyg4ometry.fluka as _fluka
 import pyg4ometry.visualisation as _vi
+import numpy as _np
 
 import pyg4ometry.test.pythonGeant4.T001_Box as _T001_Box
 
@@ -24,15 +25,19 @@ def Test() :
     b1l = _g4.LogicalVolume(b1s, bm, "b1l", reg)
     b2l = _g4.LogicalVolume(b2s, bm, "b2l", reg)
 
-    b2p1 = _g4.PhysicalVolume([0,0,0.5],[0,15,0], b2l, "b2_pv1", b1l, reg)
+    b2p1 = _g4.PhysicalVolume([0,0,_np.pi/4.0],[0,15,0], b2l, "b2_pv1", b1l, reg)
     b2p2 = _g4.PhysicalVolume([0,0,0],[0,-15,0], b2l, "b2_pv2", b1l, reg)
 
-    b1p1 = _g4.PhysicalVolume([0, 0,  -0.5],     [0, 0, -50], b1l, "b1_pv1", wl, reg)
-    b1p2 = _g4.PhysicalVolume([0, 0,  0.0],     [0, 0,   0], b1l, "b1_pv2", wl, reg)
-    b1p3 = _g4.PhysicalVolume([0, 0,  0.5],     [0, 0,  50], b1l, "b1_pv3", wl, reg)
+    b1p1 = _g4.PhysicalVolume([0, 0,  -_np.pi/4.0],     [0, 0, -50], b1l, "b1_pv1", wl, reg)
+    b1p2 = _g4.PhysicalVolume([0, 0,  0],     [0, 0,   0], b1l, "b1_pv2", wl, reg)
+    b1p3 = _g4.PhysicalVolume([0, 0,  _np.pi/4.0],     [0, 0,  50], b1l, "b1_pv3", wl, reg)
+
+    # test extent of physical volume
+    extentBB = wl.extent(includeBoundingSolid=True)
 
     v = _vi.VtkViewer()
     v.addLogicalVolume(wl)
+    v.addAxes(_vi.axesFromExtents(extentBB)[0])
     v.view()
 
     # set world volume
@@ -40,7 +45,7 @@ def Test() :
 
     # freg, nfb = _convert.geant42Fluka(wl)
 
-    freg = _convert.g2fTopLogical(wl)
+    freg = _convert.geant42FlukaLogical(wl)
 
     w = _fluka.Writer()
     w.addDetector(freg)
