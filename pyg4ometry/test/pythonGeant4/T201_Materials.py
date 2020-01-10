f612360b1e73093655a3ee3de4406f0362de3103
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2020-01-10 15:34:19 +0000

    Added tests for materials made of materials (mixtures), elements made of isotopes and composite materials made by specifying the number of element atoms (molecules)

--- pyg4ometry/test/pythonGeant4/T201_Materials.py
+++ pyg4ometry/test/pythonGeant4/T201_Materials.py
@@ -113,4 +113,131 @@ def Test_MaterialCompoundMassFraction() :
     w = _gd.Writer()
     w.addDetector(reg)
     w.write(_os.path.join(_os.path.dirname(__file__), "T201_MaterialCompound.gdml"))
-    w.writeGmadTester(_os.path.join(_os.path.dirname(__file__),"T201_MaterialCompound.gmad"),"T201_MaterialCompound.gdml")
+    w.writeGmadTester(_os.path.join(_os.path.dirname(__file__),"T201_MaterialCompound.gdml"),"T201_MaterialCompound.gdml")
+
+
+def Test_MaterialCompoundAtoms() :
+
+    reg = _g4.Registry()
+
+    # defines
+    wx = _gd.Constant("wx", "100", reg, True)
+    wy = _gd.Constant("wy", "100", reg, True)
+    wz = _gd.Constant("wz", "100", reg, True)
+
+    bx = _gd.Constant("bx", "10", reg, True)
+    by = _gd.Constant("by", "10", reg, True)
+    bz = _gd.Constant("bz", "10", reg, True)
+
+    #######################################################################################
+    wm = _g4.MaterialPredefined("G4_Galactic")
+
+    bm = _g4.MaterialCompound("water", 1.0, 2, reg)
+    he = _g4.ElementSimple("hydrogen", "H", 1, 1.01)
+    oe = _g4.ElementSimple("oxygen", "O", 8, 16.0)
+    bm.add_element_natoms(he, 2)
+    bm.add_element_natoms(oe, 1)
+    #######################################################################################
+
+    # solids
+    ws = _g4.solid.Box("ws", wx, wy, wz, reg, "mm")
+    bs = _g4.solid.Box("bs", bx, by, bz, reg, "mm")
+
+    # structure
+    wl = _g4.LogicalVolume(ws, wm, "wl", reg)
+    bl = _g4.LogicalVolume(bs, bm, "bl", reg)
+    bp = _g4.PhysicalVolume([0, 0, 0], [0, 0, 0], bl, "b_pv1", wl, reg)
+
+    # set world volume
+    reg.setWorld(wl.name)
+
+    # gdml output
+    w = _gd.Writer()
+    w.addDetector(reg)
+    w.write(_os.path.join(_os.path.dirname(__file__), "T201_MaterialCompoundAtoms.gdml"))
+    w.writeGmadTester(_os.path.join(_os.path.dirname(__file__),"T201_MaterialCompoundAtoms.gmad"),"T201_MaterialCompoundAtoms.gdml")
+
+def Test_MaterialMixture():
+
+    reg = _g4.Registry()
+
+    # defines
+    wx = _gd.Constant("wx", "100", reg, True)
+    wy = _gd.Constant("wy", "100", reg, True)
+    wz = _gd.Constant("wz", "100", reg, True)
+
+    bx = _gd.Constant("bx", "10", reg, True)
+    by = _gd.Constant("by", "10", reg, True)
+    bz = _gd.Constant("bz", "10", reg, True)
+
+    #######################################################################################
+    wm = _g4.MaterialPredefined("G4_Galactic")
+    copper = _g4.MaterialPredefined("G4_Cu")
+    zinc = _g4.MaterialPredefined("G4_Zn")
+    bm = _g4.MaterialCompound("YellowBrass_C26800", 8.14, 2, reg)
+    bm.add_material(copper, 0.67)
+    bm.add_material(zinc, 0.33)
+    #######################################################################################
+
+    # solids
+    ws = _g4.solid.Box("ws", wx, wy, wz, reg, "mm")
+    bs = _g4.solid.Box("bs", bx, by, bz, reg, "mm")
+
+    # structure
+    wl = _g4.LogicalVolume(ws, wm, "wl", reg)
+    bl = _g4.LogicalVolume(bs, bm, "bl", reg)
+    bp = _g4.PhysicalVolume([0, 0, 0], [0, 0, 0], bl, "b_pv1", wl, reg)
+
+    # set world volume
+    reg.setWorld(wl.name)
+
+    # gdml output
+    w = _gd.Writer()
+    w.addDetector(reg)
+    w.write(_os.path.join(_os.path.dirname(__file__), "T201_MaterialMixture.gdml"))
+    w.writeGmadTester(_os.path.join(_os.path.dirname(__file__),"T201_MaterialMixture.gmad"),"T201_MaterialMixture.gdml")
+
+
+def Test_MaterialIsotopes():
+
+    reg = _g4.Registry()
+
+    # defines
+    wx = _gd.Constant("wx", "100", reg, True)
+    wy = _gd.Constant("wy", "100", reg, True)
+    wz = _gd.Constant("wz", "100", reg, True)
+
+    bx = _gd.Constant("bx", "10", reg, True)
+    by = _gd.Constant("by", "10", reg, True)
+    bz = _gd.Constant("bz", "10", reg, True)
+
+    #######################################################################################
+    wm = _g4.MaterialPredefined("G4_Galactic")
+
+    u235 = _g4.Isotope("U235", 92, 235, 235.044)
+    u238 = _g4.Isotope("U238", 92, 238, 238.051)
+    uranium = _g4.ElementIsotopeMixture("uranium", "U", 2)
+    uranium.add_isotope(u235, 0.00716)
+    uranium.add_isotope(u238, 0.99284)
+
+    bm = _g4.MaterialCompound("natural_uranium", 19.1, 2, reg)
+    bm.add_element_massfraction(uranium, 1)
+    #######################################################################################
+
+    # solids
+    ws = _g4.solid.Box("ws", wx, wy, wz, reg, "mm")
+    bs = _g4.solid.Box("bs", bx, by, bz, reg, "mm")
+
+    # structure
+    wl = _g4.LogicalVolume(ws, wm, "wl", reg)
+    bl = _g4.LogicalVolume(bs, bm, "bl", reg)
+    bp = _g4.PhysicalVolume([0, 0, 0], [0, 0, 0], bl, "b_pv1", wl, reg)
+
+    # set world volume
+    reg.setWorld(wl.name)
+
+    # gdml output
+    w = _gd.Writer()
+    w.addDetector(reg)
+    w.write(_os.path.join(_os.path.dirname(__file__), "T201_MaterialIsotopes.gdml"))
+    w.writeGmadTester(_os.path.join(_os.path.dirname(__file__),"T201_MaterialIsotopes.gmad"),"T201_MaterialIsotopes.gdml")
