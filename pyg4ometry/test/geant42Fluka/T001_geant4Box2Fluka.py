42a3a089410b7258fe6cca0d4489372196cfbb6d
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2020-02-02 11:25:13 +0000

    Working placement/boolean geant4 to fluka conversion

--- pyg4ometry/test/geant42Fluka/T001_geant4Box2Fluka.py
+++ pyg4ometry/test/geant42Fluka/T001_geant4Box2Fluka.py
@@ -1,5 +1,6 @@
 import os as _os
 import pyg4ometry.geant4 as _g4
+import pyg4ometry.gdml as _gd
 import pyg4ometry.convert as _convert
 import pyg4ometry.fluka as _fluka
 import pyg4ometry.visualisation as _vi
@@ -13,9 +14,9 @@ def Test(vis = True, interactive = False) :
     reg = _g4.Registry()
 
     # solids
-    ws = _g4.solid.Box("ws", 200, 200, 200, reg, "mm")
-    b1s = _g4.solid.Box("b1s", 50,   50,  50, reg, "mm")
-    b2s = _g4.solid.Box("b2s", 10,   10,  10, reg, "mm")
+    ws = _g4.solid.Box("ws", 1000, 1000, 1000, reg, "mm")
+    b1s = _g4.solid.Box("b1s", 50,   75,  100, reg, "mm")
+    b2s = _g4.solid.Box("b2s", 5,   10,  15, reg, "mm")
 
     # materials
     wm = _g4.MaterialPredefined("G4_Galactic")
@@ -26,12 +27,13 @@ def Test(vis = True, interactive = False) :
     b1l = _g4.LogicalVolume(b1s, bm, "b1l", reg)
     b2l = _g4.LogicalVolume(b2s, bm, "b2l", reg)
 
-    b2p1 = _g4.PhysicalVolume([0,0,_np.pi/4.0],[0,15,0], b2l, "b2_pv1", b1l, reg)
-    b2p2 = _g4.PhysicalVolume([0,0,0],[0,-15,0], b2l, "b2_pv2", b1l, reg)
+    b2p1 = _g4.PhysicalVolume([0,0,_np.pi/4.0],[0, 15,0], b2l, "b2_pv1", b1l, reg)
+    b2p2 = _g4.PhysicalVolume([0,0,0]         ,[0,-15,0], b2l, "b2_pv2", b1l, reg)
 
-    b1p1 = _g4.PhysicalVolume([0, 0,  -_np.pi/4.0],     [0, 0, -50], b1l, "b1_pv1", wl, reg)
-    b1p2 = _g4.PhysicalVolume([0, 0,  0],     [0, 0,   0], b1l, "b1_pv2", wl, reg)
-    b1p3 = _g4.PhysicalVolume([0, 0,  _np.pi/4.0],     [0, 0,  50], b1l, "b1_pv3", wl, reg)
+    b1p1 = _g4.PhysicalVolume([0,0,0]         , [0, 0,     -300], b1l, "b1_pv1", wl, reg)
+    b1p2 = _g4.PhysicalVolume([_np.pi/4.0, 0,  0],  [0, 0, -100], b1l, "b1_pv2", wl, reg)
+    b1p3 = _g4.PhysicalVolume([0, _np.pi/4.0,  0],  [0, 0,  100], b1l, "b1_pv3", wl, reg)
+    b1p4 = _g4.PhysicalVolume([0,  0, _np.pi/4.0],  [0, 0,  300], b1l, "b1_pv4", wl, reg)
 
     # set world volume
     reg.setWorld(wl.name)
