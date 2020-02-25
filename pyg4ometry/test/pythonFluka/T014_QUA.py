e303c907b76c70de6d003f40e5600268ae52c1d6
swalker92 <zwap082@live.rhul.ac.uk>
2020-02-25 11:49:55 +0000

    better QUA python test

--- pyg4ometry/test/pythonFluka/T014_QUA.py
+++ pyg4ometry/test/pythonFluka/T014_QUA.py
@@ -5,15 +5,15 @@ from pyg4ometry.fluka import QUA, Region, Zone, FlukaRegistry, Extent, XYP, XZP
 def Test(vis=False, interactive=False) :
     freg = FlukaRegistry()
 
-
-
     parabolicCylinder = QUA("parab",
-                            0.006, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, -20,
+                            0.006, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, -200,
                             flukaregistry=freg)
 
-    end1 = XYP("end1", 20, flukaregistry=freg)
+
+    # 1 metre long parabolic cylinder 10cm tall from base to tip.
+    end1 = XYP("end1", 1000, flukaregistry=freg)
     end2 = XYP("end2",  0, flukaregistry=freg)
-    end3 = XZP("end3", -30, flukaregistry=freg)
+    end3 = XZP("end3", 100, flukaregistry=freg)
 
     z = Zone()
     z.addIntersection(parabolicCylinder)
@@ -25,7 +25,7 @@ def Test(vis=False, interactive=False) :
     region.addZone(z)
     freg.addRegion(region)
 
-    quaExtent = {"QUA_REG": Extent([-150., 0., 0], [150., 200., 200.])}
+    quaExtent = {"QUA_REG": Extent([-150., 100., 0], [150., 200., 1000.])}
 
     greg = convert.fluka2Geant4(freg,
                                 quadricRegionExtents=quaExtent)
@@ -33,7 +33,7 @@ def Test(vis=False, interactive=False) :
     v = None
     if vis:
         v = vi.VtkViewer()
-        v.addAxes(length=20)
+        v.addAxes(origin=[0, 100, 0], length=100)
         v.addLogicalVolume(greg.getWorldVolume())
         v.view(interactive=interactive)
 
