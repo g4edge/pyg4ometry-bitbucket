0089e2de444624dc895854f6739aea1d14022363
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2020-04-15 18:53:47 +0100

    Build cgal extension

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup, find_packages
+from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
 from Cython.Build import cythonize
+
+
+class ctypes(Extension): pass
+
+exts = cythonize(["pyg4ometry/pycsg/geom.pyx","pyg4ometry/pycsg/core.pyx"])
+exts.append(ctypes('pyg4ometry.pycgal.pyg4_cgal', 
+                   sources=['./pyg4ometry/pycgal/pyg4_cgal.cpp'],
+                   include_dirs = ['/opt/local/include'],
+                   library_dirs = ['/opt/local/lib'],
+                   libraries = ['CGAL','mpfr','gmp','boost_thread-mt','stdc++']))
 
 setup(
     name="pyg4ometry",
-    version="0.2.0",
+    version="0.9.0",
     packages=find_packages(exclude=["docs", "tests"]),
 
     # Only tested with version 4.7.
@@ -16,8 +28,8 @@ setup(
                       "GitPython",
                       "testtools"],
     # cython, pyqt5
-
-    ext_modules=cythonize(["pyg4ometry/pycsg/geom.pyx","pyg4ometry/pycsg/core.pyx"]),
+    
+    ext_modules=exts,
 
     python_requires="==2.7.*", # refer to pep440 for writing these correctly
 
