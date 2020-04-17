00c9edcff5368bd88638aea01344688f5ff88cd1
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2020-04-17 12:56:59 +0100

    Add a test for pressure and temperature setting for materials.

--- pyg4ometry/test/pythonGeant4/T201_Materials.py
+++ pyg4ometry/test/pythonGeant4/T201_Materials.py
@@ -250,3 +250,49 @@ def Test_MaterialIsotopes():
     w.addDetector(reg)
     w.write(_os.path.join(_os.path.dirname(__file__), "T201_MaterialIsotopes.gdml"))
     w.writeGmadTester(_os.path.join(_os.path.dirname(__file__),"T201_MaterialIsotopes.gmad"),"T201_MaterialIsotopes.gdml")
+
+
+
+def Test_MaterialPressureTemperature() :
+
+    reg = _g4.Registry()
+
+    # defines
+    wx = _gd.Constant("wx", "100", reg, True)
+    wy = _gd.Constant("wy", "100", reg, True)
+    wz = _gd.Constant("wz", "100", reg, True)
+
+    bx = _gd.Constant("bx", "10", reg, True)
+    by = _gd.Constant("by", "10", reg, True)
+    bz = _gd.Constant("bz", "10", reg, True)
+
+    #######################################################################################
+    wm = _g4.MaterialCompound("air",1.290e-3,2,reg)
+    ne = _g4.ElementSimple("nitrogen","N",7,14.01)
+    oe = _g4.ElementSimple("oxygen","O",8,16.0)
+    wm.add_element_massfraction(ne,0.7)
+    wm.add_element_massfraction(oe,0.3)
+    wm.set_pressure(1100, "pascal")
+    wm.set_temperature(293.15, "K")
+
+    bm = _g4.MaterialSingleElement("iron",26,55.8452,7.874,reg)    # iron at near room temp
+    #######################################################################################
+
+    # solids
+    ws = _g4.solid.Box("ws", wx, wy, wz, reg, "mm")
+    bs = _g4.solid.Box("bs", bx, by, bz, reg, "mm")
+
+    # structure
+    wl = _g4.LogicalVolume(ws, wm, "wl", reg)
+    bl = _g4.LogicalVolume(bs, bm, "bl", reg)
+    bp = _g4.PhysicalVolume([0, 0, 0], [0, 0, 0], bl, "b_pv1", wl, reg)
+
+    # set world volume
+    reg.setWorld(wl.name)
+
+    # gdml output
+    w = _gd.Writer()
+    w.addDetector(reg)
+    w.write(_os.path.join(_os.path.dirname(__file__), "T201_MaterialPressureTemperature.gdml"))
+    w.writeGmadTester(_os.path.join(_os.path.dirname(__file__),"T201_MaterialPressureTemperature.gmad"),"T201_MaterialPressureTemperature.gdml")
+
