a2eb4095a36eeaf273234ee16b456f571adbfa4d
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2020-04-21 13:02:15 +0100

    Added placement code and example of sector bend

--- pyg4ometry/test/freecadExamples/FreecadLoadingTests.py
+++ pyg4ometry/test/freecadExamples/FreecadLoadingTests.py
@@ -1,7 +1,9 @@
 import unittest as _unittest
 
 import pyg4ometry
-import pyg4ometry.freecad as _fc
+import pyg4ometry.gdml as _gd
+import pyg4ometry.geant4 as _g4
+
 
 import logging as _log
 import os as _os
@@ -49,6 +51,53 @@ def LoadFreecad(fileName, interactive = False) :
     writer.addDetector(registry)
     writer.write(newFilename)
 
+    return True
+
+def LoadFreecad_withPlacement(fileName, interactive=False) :
+
+    # Loading
+    reader = pyg4ometry.freecad.Reader(fileName)
+
+    # Get registry
+    reg = reader.getRegistry()
+
+    # Convert
+    reader.relabelModel()
+    reader.convertFlat()
+    registry = reader.getRegistry()
+
+    # CAD logical
+    logical = registry.getWorldVolume()
+
+    # test extent of physical volume
+    extentBB = logical.extent(includeBoundingSolid=True)
+
+    # create world
+    # defines
+    wx = _gd.Constant("wx", 2*(extentBB[1][0]-extentBB[0][0]), reg, True)
+    wy = _gd.Constant("wy", 2*(extentBB[1][1]-extentBB[0][2]), reg, True)
+    wz = _gd.Constant("wz", 2*(extentBB[1][2]-extentBB[0][2]), reg, True)
+    wm = _g4.MaterialPredefined("G4_Galactic")
+
+    ws = _g4.solid.Box("ws",wx,wy,wz, reg, "mm")
+    wl = _g4.LogicalVolume(ws, wm, "wl", reg)
+    bp = _g4.PhysicalVolume([0,0,0],reader.rootPlacement,logical, "cad_pv1", wl, reg)
+
+    # Visualisation
+    v = pyg4ometry.visualisation.VtkViewer()
+    v.addLogicalVolume(wl)
+    v.addAxes(pyg4ometry.visualisation.axesFromExtents(extentBB)[0])
+    v.view(interactive=interactive)
+
+    # Writing
+    if fileName.find("stp") != -1 :
+        newFilename = fileName.replace(".stp", ".gdml")
+    else :
+        newFilename = fileName.replace(".step", ".gdml")
+
+    writer = pyg4ometry.gdml.Writer()
+    writer.addDetector(registry)
+    writer.write(newFilename)
 
     return True
 
