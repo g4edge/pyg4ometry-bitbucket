30ab94f35d585fd1b2b651ba8b88120213922da6
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2020-04-25 09:22:25 +0000

    config parser for GitPython

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
                       "vtk",
                       "cython",
                       "GitPython",
+                      "configparser",
                       "testtools",
                       "pypandoc"],
     # cython, pyqt5
