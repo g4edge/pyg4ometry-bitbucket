0e4b3d41d60314dd4166b80ffceb672b7cd3fd13
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2020-04-30 22:20:43 +0100

    Coverage in a better location

--- coverage/runCoverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-# 1150.988s 2019/10/27
-
-coverage-2.7 run --source pyg4ometry --omit="*test*","*gui*","*RegionLexer*","*RegionParser*","*RegionParserVisitor*","*GdmlExpressionLexer*","*GdmlExpressionParser*","*GdmlExpressionParserVisitor*" -m ../pyg4ometry/test/runTests.py
-coverage-2.7 report -m > runCoverage.log
