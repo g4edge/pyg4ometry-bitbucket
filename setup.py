3490deb732060f8f1d9498e8abf30c8edae76dbb
swalker92 <zwap082@live.rhul.ac.uk>
2020-07-14 11:09:11 +0100

    fix failing install when user is missing setuptools or cython
    setup.py imports cython and setuptools so adding it to install_requires
    won't work.  This is the official way to work around this issue, that of
    requirements for the setup.py itself.

--- setup.py
+++ setup.py
@@ -1,35 +1,8 @@
 from setuptools import find_packages
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
-#import os
-#os.environ["CC"] = "gcc-9"
-#os.environ["CXX"] = "g++-9"
-
-class ctypes(Extension):
-    pass
-
-# might not have cython before running setup
-try:
-    from Cython.Build import cythonize
-except ImportError:
-     def cythonize(*args, **kwargs):
-         from Cython.Build import cythonize
-         return cythonize(*args, **kwargs)
 
 exts = cythonize(["pyg4ometry/pycsg/geom.pyx", "pyg4ometry/pycsg/core.pyx"])
-#exts.append(ctypes('pyg4ometry.pycgal.pyg4_cgal', 
-#                   sources=['./pyg4ometry/pycgal/pyg4_cgal.cpp'],
-#                   include_dirs = ['/opt/local/include', '/usr/local/include'],
-#                   library_dirs = ['/usr/local/lib'],
-#                   libraries = ['mpfr','gmp','boost_thread-mt','stdc++']))
-
-#try:
-#    import pypandoc
-#    long_description = pypandoc.convert_file("README.md", "rst")
-#except ImportError:
-#    print ("Warning: pypandoc module not found, could not convert"
-#           " Markdown to reStructuredText." )
-#    long_description = ""
 
 setup(
     name="pyg4ometry",
@@ -55,13 +28,11 @@ setup(
                       "testtools",
                       "pypandoc",
                       "ipython"],
-    # pyqt5
     ext_modules=exts,
-    python_requires=">=3.7.1", # refer to pep440 for writing these correctly
+    python_requires=">=3.7.1",
     author="Stewart T. Boogert",
     author_email="stewart.boogert@rhul.ac.uk",
     description='Geometry package for high energy physics (Geant4, Fluka)',
-    #  longdescription=longdescription,
     license='GPL3',
     url='https://bitbucket.org/jairhul/pyg4ometry/',
     keywords='geometry bdsim particle physics accelerators',
