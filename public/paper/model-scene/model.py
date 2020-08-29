1a3b7308ad79d02a9132c1e9ec4e173b6eceaaf7
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2020-08-29 23:19:33 +0100

    Updated complete model figure

--- public/paper/model-scene/model.py
+++ public/paper/model-scene/model.py
@@ -8,10 +8,13 @@ def buildModel(vis = True, write = True, render = True) :
     world_material = pyg4ometry.geant4.MaterialPredefined("G4_Galactic")
     world_solid    = pyg4ometry.geant4.solid.Box("world_solid", 10000, 10000, 10000, reg, "mm")
     world_logical  = pyg4ometry.geant4.LogicalVolume(world_solid, world_material, "world_logical", reg)
+    print(world_logical.name)
 
     # load gun chamber 
     reader_gunChamber   = pyg4ometry.gdml.Reader("./CuboidalChamber.gdml")
     gunChamber_logical  = reader_gunChamber.getRegistry().getWorldVolume()
+    gunChamber_logical.name = "gunChamber_lv"
+    print("gun chamber ",gunChamber_logical.name)
     gunChamber_assembly = gunChamber_logical.assemblyVolume()
     gunChamber_physical = pyg4ometry.geant4.PhysicalVolume([0,0,0],
                                                            [0,0,0],
@@ -24,14 +27,16 @@ def buildModel(vis = True, write = True, render = True) :
 
     reg.addVolumeRecursive(gunChamber_physical)
 
-    # load gate 
+    # load gate
     reader_gateValve  = pyg4ometry.stl.Reader("./GV-100CF-C-M.stl")
     gateValve_solid   = reader_gateValve.getSolid()
-    gateValve_material= pyg4ometry.geant4.MaterialPredefined("G4_Fe")     
+    gateValve_material= pyg4ometry.geant4.MaterialPredefined("G4_Fe")
     gateValve_logical = pyg4ometry.geant4.LogicalVolume(gateValve_solid,
                                                         gateValve_material,
                                                         "gateValve_logical",
                                                         reg)
+    gateValve_logical.name = "gateValve_lv"
+    print("gate chamber",gateValve_logical.name)
     gateValve_assembly= gateValve_logical.assemblyVolume()
     # gateValve_logical.clipSolid()
     print("gate value", gateValve_logical.extent(includeBoundingSolid=True))
@@ -41,12 +46,14 @@ def buildModel(vis = True, write = True, render = True) :
                                                           "gateValve_physical",
                                                           world_logical,
                                                           reg,
-                                                          False)     
+                                                          False)
     reg.addVolumeRecursive(gateValve_physical)
 
     # load magnets
     reader_triplet = pyg4ometry.gdml.Reader("./quad_triplet.gdml")
     triplet_logical = reader_triplet.getRegistry().getWorldVolume()
+    triplet_logical.name = "triplet_lv"
+    print("triplet ",triplet_logical.name)
     triplet_assembly = triplet_logical.assemblyVolume()
     triplet_physical = pyg4ometry.geant4.PhysicalVolume([0,0,0],
                                                         [0,0,900+2*38.9001],
@@ -60,7 +67,7 @@ def buildModel(vis = True, write = True, render = True) :
     reg.addVolumeRecursive(triplet_physical,"reuse")
 
 
-    # load cad dipole 
+    # load cad dipole
     reader_dipole = pyg4ometry.freecad.Reader("./10_SectorBendMedium.step")
     reader_dipole.relabelModel()
     reader_dipole.convertFlat()
@@ -73,6 +80,8 @@ def buildModel(vis = True, write = True, render = True) :
 
     dipole_placement = reader_dipole.rootPlacement
     dipole_logical  = reader_dipole.getRegistry().getWorldVolume()
+    dipole_logical.name = "dipole_lv"
+    print("dipole ",dipole_logical.name)
     dipole_assembly = dipole_logical.assemblyVolume()
     # dipole_logical.clipSolid()
     dipole_physical = pyg4ometry.geant4.PhysicalVolume([0,0,0],
@@ -83,12 +92,14 @@ def buildModel(vis = True, write = True, render = True) :
                                                        reg,
                                                        addRegistry=False)
     print("dipole",dipole_logical.extent(includeBoundingSolid=True))
-    reg.addVolumeRecursive(dipole_physical,"reuse") 
+    reg.addVolumeRecursive(dipole_physical,"reuse")
 
     # load faraday cup
     reader_faraday  = pyg4ometry.fluka.Reader("faradayCup2.inp")
     faraday_greg    = pyg4ometry.convert.fluka2Geant4(reader_faraday.flukaregistry)
     faraday_logical = faraday_greg.getWorldVolume()
+    faraday_logical.name = "faraday_lv"
+    print("faraday ",faraday_logical.name)
     faraday_assembly = faraday_logical.assemblyVolume()
 
     faraday_physical = pyg4ometry.geant4.PhysicalVolume([0,0,0],
