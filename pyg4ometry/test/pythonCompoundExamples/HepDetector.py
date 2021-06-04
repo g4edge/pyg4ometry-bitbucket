8ee447bf7ced810867f80693168274d6d64e5464
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2021-06-04 19:56:27 +0100

    Fix module name clashes

--- pyg4ometry/test/pythonCompoundExamples/HepDetector.py
+++ pyg4ometry/test/pythonCompoundExamples/HepDetector.py
@@ -4,25 +4,63 @@ import numpy as _np
 def HepDetector() :
     pass
 
-def SiTrackerBarrelRing(radius = 0.25, sensorSize= 0.08, nAzimuth = 25, tiltAngleDeg = 11, reg = None) :
+
+def SiTracker() :
+    reg = pyg4ometry.geant4.Registry()
+
+    worldBox      = pyg4ometry.geant4.solid.Box("worldBox",10000,10000,10000,reg,"mm")
+    worldLv       = pyg4ometry.geant4.LogicalVolume(worldBox,"G4_Galactic","worldLv",reg)
+
+    siTrackerTubs = pyg4ometry.geant4.solid.Tubs("siTrackerTubs",100,500,1650,0,2*_np.pi,reg,"mm","rad")
+    siTrackerLv   = pyg4ometry.geant4.LogicalVolume(siTrackerTubs,"G4_Galactic","siTrackerLv",reg)
+    siTrackerPv   = pyg4ometry.geant4.PhysicalVolume([0,0,0],[0,0,0],siTrackerLv,"siTrackerPv",worldLv,reg)
+
+    siTrackerModuleAv = SiTrackerBarrelModule(reg=reg)
+
+    siTrackerLayer1Av = SiTrackerBarrelLayer(name = "barrelAv1", moduleAv=siTrackerModuleAv, radius = 0.15, nAzimuth = 25, reg = reg)
+    siTrackerLayer1Pv = pyg4ometry.geant4.PhysicalVolume([_np.pi/2.0,0,0],[0,0,0],siTrackerLayer1Av,"siTrackerLayer1Pv",siTrackerLv, reg)
+
+    siTrackerLayer2Av = SiTrackerBarrelLayer(name = "barrelAv2", moduleAv=siTrackerModuleAv, radius = 0.30, nAzimuth = int(0.3/0.15*25), reg = reg)
+    siTrackerLayer2Pv = pyg4ometry.geant4.PhysicalVolume([_np.pi/2.0,0,0],[0,0,0],siTrackerLayer2Av,"siTrackerLayer2Pv",siTrackerLv, reg)
+
+    siTrackerLayer3Av = SiTrackerBarrelLayer(name = "barrelAv3", moduleAv=siTrackerModuleAv, radius = 0.45, nAzimuth = int(0.45/0.15*25), reg = reg)
+    siTrackerLayer3Pv = pyg4ometry.geant4.PhysicalVolume([_np.pi/2.0,0,0],[0,0,0],siTrackerLayer3Av,"siTrackerLayer3Pv",siTrackerLv, reg)
+
+    v = pyg4ometry.visualisation.VtkViewer()
+    v.addLogicalVolume(worldLv)
+    v.view()
+
+    # gdml output
+    reg.setWorld("worldLv")
+    w = pyg4ometry.gdml.Writer()
+    w.addDetector(reg)
+    w.write("SiTracker.gdml")
+
+def SiTrackerBarrelLayer(name = "barrelAv", moduleAv = None,
+                         length = 1.6, radius = 0.25, sensorSize= 0.08, nAzimuth = 25, tiltAngleDeg = 11,
+                         reg = None) :
 
     if reg is None :
         reg = pyg4ometry.geant4.Registry()
 
     tiltAngleRad = pyg4ometry.transformation.deg2rad(tiltAngleDeg)
 
-    sitbmLv = SiTrackerBarrelModule(sensorSize = sensorSize)
+    sitbmLv = moduleAv
 
-    barrelAv = pyg4ometry.geant4.AssemblyVolume("barrelAv",reg,True)
+    barrelAv = pyg4ometry.geant4.AssemblyVolume(name,reg,True)
 
-    for i in range(0,nAzimuth,1) :
-        phi = 2*_np.pi/nAzimuth*i
-        modulePv = pyg4ometry.geant4.PhysicalVolume([0, phi+_np.pi/2+tiltAngleRad, 0],
-                                                    [radius*_np.cos(phi), 0, radius*_np.sin(phi),"m"],
-                                                    sitbmLv,
-                                                    "modulePv"+str(i),
-                                                    barrelAv, reg)
+    nLength = int(length/(2*sensorSize))
 
+    for i in range(0,nLength, 1) :
+        z = i*2*sensorSize- length/2.0 + sensorSize
+
+        for j in range(0,nAzimuth,1) :
+            phi = 2*_np.pi/nAzimuth*j
+            modulePv = pyg4ometry.geant4.PhysicalVolume([0, phi+_np.pi/2+tiltAngleRad, 0],
+                                                        [radius*_np.cos(phi), z, radius*_np.sin(phi),"m"],
+                                                         sitbmLv,
+                                                        name+"_modulePv"+str(i)+"_"+str(j),
+                                                        barrelAv, reg)
 
     return barrelAv
 
@@ -63,7 +101,6 @@ def SiTrackerEndLayer(innerRadius = 0.3, outerRadius = 0.56, nPetal = 20, phiPet
     pass
 
 def SiTrackerEndModule() :
-
     pass
 
 def SiTrackerEndSensor(innerRadius = 0.3, outerRadius = 0.56) :
