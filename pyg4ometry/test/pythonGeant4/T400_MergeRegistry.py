59f03b3615b7f82113e19b321da0a68908741802
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-07-26 13:45:01 +0200

    Extend test also for material resolution in registry merging.

--- pyg4ometry/test/pythonGeant4/T400_MergeRegistry.py
+++ pyg4ometry/test/pythonGeant4/T400_MergeRegistry.py
@@ -3,7 +3,7 @@ import pyg4ometry.gdml as _gd
 import pyg4ometry.geant4 as _g4
 import pyg4ometry.visualisation as _vi
 
-def MakeGeometry(size=50) :
+def MakeGeometry(size=50, lowOxygen=False) :
     reg = _g4.Registry()
 
     # defines
@@ -11,12 +11,25 @@ def MakeGeometry(size=50) :
     wy = _gd.Constant("wy", size, reg, True)
     wz = _gd.Constant("wz", size, reg, True)
 
+    # "size" argument allows us to define different sizes with the same name
+    # to test resolution when we merge
     bx = _gd.Constant("bx", size/5, reg, True)
     by = _gd.Constant("by", size/5, reg, True)
     bz = _gd.Constant("bz", size/5, reg, True)
 
     wm = _g4.MaterialPredefined("G4_Galactic")
-    bm = _g4.MaterialPredefined("G4_Fe")
+
+    # this allows us to define two technically different materials but called
+    # "air" so they should be resolved correctly in the merging
+    air = _g4.MaterialCompound("air",1.290e-3,2,reg)
+    ne = _g4.ElementSimple("nitrogen","N",7,14.01)
+    oe = _g4.ElementSimple("oxygen","O",8,16.0)
+    if lowOxygen:
+        air.add_element_massfraction(ne,0.95)
+        air.add_element_massfraction(oe,0.05)
+    else:
+        air.add_element_massfraction(ne,0.7)
+        air.add_element_massfraction(oe,0.3)
 
     # solids
     ws = _g4.solid.Box("ws", wx, wy, wz, reg, "mm")
@@ -24,7 +37,7 @@ def MakeGeometry(size=50) :
 
     # structure
     wl = _g4.LogicalVolume(ws, wm, "wl", reg)
-    bl = _g4.LogicalVolume(bs, bm, "bl", reg)
+    bl = _g4.LogicalVolume(bs, air, "bl", reg)
     bp = _g4.PhysicalVolume([0, 0, 0], [0, 0, 0], bl, "b_pv1", wl, reg)
 
     # set world volume
@@ -36,7 +49,7 @@ def MakeGeometry(size=50) :
 def Test(vis = False, interactive=False) :
     reg0 = _g4.Registry()
     reg1 = MakeGeometry()
-    reg2 = MakeGeometry(size=60)
+    reg2 = MakeGeometry(size=70, lowOxygen=True)
 
     l1 = reg1.getWorldVolume()
     l2 = reg2.getWorldVolume()
