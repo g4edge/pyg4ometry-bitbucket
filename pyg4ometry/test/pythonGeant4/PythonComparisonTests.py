37fb6a2792195469915a3e038e34663efc32313f
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-10-05 22:42:45 +0200

    Lay out test files.

--- pyg4ometry/test/pythonGeant4/PythonComparisonTests.py
+++ pyg4ometry/test/pythonGeant4/PythonComparisonTests.py
@@ -5,6 +5,13 @@ import numpy as _np
 import pyg4ometry
 
 from . import T700_ComparisonMaterial
+from . import T701_ComparisonSolid
+from . import T702_ComparisonLogicalVolume
+from . import T703_ComparisonPhysicalVolume
+from . import T704_ComparisonAssemblyVolume
+from . import T705_ComparisonReplicaVolume
+from . import T706_ComparisonDivisionVolume
+from . import T707_ComparisonParameterisedVolume
 
 import logging as _log
 
@@ -12,12 +19,29 @@ import logging as _log
 #logger.disabled = True
 
 class PythonComparisonTests(_unittest.TestCase):
-
     def test_PythonComparison_T700_ComparisonMaterial(self):
         self.assertTrue(T700_ComparisonMaterial.Test())
 
+    def test_PythonComparison_T701_ComparisonSolid(self):
+        self.assertTrue(T701_ComparisonSolid.Test())
 
+    def test_PythonComparison_T702_ComparisonLogicalVolume(self):
+        self.assertTrue(T702_ComparisonLogicalVolume.Test())
 
+    def test_PythonComparison_T703_ComparisonPhysicalVolume(self):
+        self.assertTrue(T703_ComparisonPhysicalVolume.Test())
+
+    def test_PythonComparison_T704_ComparisonAssemblyVolume(self):
+        self.assertTrue(T704_ComparisonAssemblyVolume.Test())
+
+    def test_PythonComparison_T705_ComparisonReplicaVolume(self):
+        self.assertTrue(T705_ComparisonReplicaVolume.Test())
+
+    def test_PythonComparison_T706_ComparisonDivisionVolume(self):
+        self.assertTrue(T706_ComparisonDivisionVolume.Test())
+
+    def test_PythonComparison_T707_ComparisonParameterisedVolume(self):
+        self.assertTrue(T707_ComparisonParameterisedVolume.Test())
 
 if __name__ == '__main__':
     _unittest.main(verbosity=2)
