ec0369086b5bd97afd0d8a9a12e92f42fc0acd89
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-10-06 18:10:59 +0200

    PV tests are actually done in LV tests really.

--- pyg4ometry/test/pythonGeant4/PythonComparisonTests.py
+++ pyg4ometry/test/pythonGeant4/PythonComparisonTests.py
@@ -7,7 +7,6 @@ import pyg4ometry
 from . import T700_ComparisonMaterial
 from . import T701_ComparisonSolid
 from . import T702_ComparisonLogicalVolume
-from . import T703_ComparisonPhysicalVolume
 from . import T704_ComparisonAssemblyVolume
 from . import T705_ComparisonReplicaVolume
 from . import T706_ComparisonDivisionVolume
@@ -27,9 +26,6 @@ class PythonComparisonTests(_unittest.TestCase):
 
     def test_PythonComparison_T702_ComparisonLogicalVolume(self):
         self.assertTrue(T702_ComparisonLogicalVolume.Test())
-
-    def test_PythonComparison_T703_ComparisonPhysicalVolume(self):
-        self.assertTrue(T703_ComparisonPhysicalVolume.Test())
 
     def test_PythonComparison_T704_ComparisonAssemblyVolume(self):
         self.assertTrue(T704_ComparisonAssemblyVolume.Test())
