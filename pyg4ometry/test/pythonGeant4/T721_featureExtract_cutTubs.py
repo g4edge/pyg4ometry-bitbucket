f9264a98c7662c64e46bd185d84351fcf8590bc2
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-08-10 10:25:37 +0100

    Random markers for features

--- pyg4ometry/test/pythonGeant4/T721_featureExtract_cutTubs.py
+++ pyg4ometry/test/pythonGeant4/T721_featureExtract_cutTubs.py
@@ -7,14 +7,26 @@ def Test(vis=True, interactive=True):
     reg = _pyg4.geant4.Registry()
     radius1 = 7
     radius2 = 9
-    t = _pyg4.geant4.solid.CutTubs("t1", radius1, radius2, 50, 0, 2 * _np.pi, [0, 0, -1], [0, 0, 1], reg)
+    theta = 0.25
+    rho = 500
+
+    s = theta*rho
+    d = _np.sin(theta/2.)*rho*2
+
+    n1 = [_np.cos(_np.pi/2-theta/2.0),0,-_np.sin(_np.pi/2-theta/2.0)]
+    n2 = [_np.cos(_np.pi/2-theta/2.0),0, _np.sin(_np.pi/2-theta/2.0)]
+
+    print(n1)
+    print(n2)
+
+    t = _pyg4.geant4.solid.CutTubs("t1", radius1, radius2, d, 0, 2 * _np.pi,n1,n2,reg)
 
     stlFileName = _os.path.join(_os.path.dirname(__file__), "T721_featureExtract_cutTubs.stl")
     datFileName = stlFileName.replace("stl","dat")
     _pyg4.convert.pycsgMeshToStl(t.mesh(),stlFileName)
 
     p1 = _pyg4.features.algos.Plane([0,0,0],[0,0,1])
-    r = _pyg4.features.extract(stlFileName, circumference=2*_np.pi*8, planes=[p1], outputFileName=datFileName, bViewer=vis)
+    r = _pyg4.features.extract(stlFileName, angle = 45, circumference=2*_np.pi*8, planes=[p1], outputFileName=datFileName, bViewer=vis)
 
     fd = _pyg4.features.algos.FeatureData()
     fd.readFile(datFileName)
