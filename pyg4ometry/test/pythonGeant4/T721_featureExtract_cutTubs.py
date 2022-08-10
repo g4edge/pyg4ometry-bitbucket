7a2d38adcad57ce5077325e254868aec1edec68e
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-08-10 18:26:41 +0100

    WIP : Working coordinate system extractor

--- pyg4ometry/test/pythonGeant4/T721_featureExtract_cutTubs.py
+++ pyg4ometry/test/pythonGeant4/T721_featureExtract_cutTubs.py
@@ -7,7 +7,7 @@ def Test(vis=True, interactive=True):
     reg = _pyg4.geant4.Registry()
     radius1 = 7
     radius2 = 9
-    theta = 0.25
+    theta = 0.1
     rho = 500
 
     s = theta*rho
@@ -16,9 +16,6 @@ def Test(vis=True, interactive=True):
     n1 = [_np.cos(_np.pi/2-theta/2.0),0,-_np.sin(_np.pi/2-theta/2.0)]
     n2 = [_np.cos(_np.pi/2-theta/2.0),0, _np.sin(_np.pi/2-theta/2.0)]
 
-    print(n1)
-    print(n2)
-
     t = _pyg4.geant4.solid.CutTubs("t1", radius1, radius2, d, 0, 2 * _np.pi,n1,n2,reg)
 
     stlFileName = _os.path.join(_os.path.dirname(__file__), "T721_featureExtract_cutTubs.stl")
@@ -26,10 +23,34 @@ def Test(vis=True, interactive=True):
     _pyg4.convert.pycsgMeshToStl(t.mesh(),stlFileName)
 
     p1 = _pyg4.features.algos.Plane([0,0,0],[0,0,1])
-    r = _pyg4.features.extract(stlFileName, angle = 45, circumference=2*_np.pi*8, planes=[p1], outputFileName=datFileName, bViewer=vis)
+    v = _pyg4.features.extract(stlFileName,
+                               angle = 46,
+                               circumference=2*_np.pi*8,
+                               planes=[],
+                               outputFileName=datFileName,
+                               bViewer=vis,
+                               bViewerInteractive=False)
 
     fd = _pyg4.features.algos.FeatureData()
     fd.readFile(datFileName)
+
+    p1 = fd.features[2]["plane"]
+    p2 = fd.features[3]["plane"]
+
+    pp1 = _pyg4.features.Plane(p1[0:3], p1[3:])
+    pp2 = _pyg4.features.Plane(p2[0:3], p2[3:])
+    pp3 = _pyg4.features.Plane([0, 0, 0], [0, 1, 0])
+
+    cs = _pyg4.features.CoordinateSystem()
+    cs.makeFromPlanes(pp1, pp2, pp3)
+
+    cs1 = cs.coordinateSystem(0,0.1,0)
+
+    v.addPlane(cs.origin, cs.e1, cs.e2, cs.dist)
+    v.addPlane(cs.origin, cs1.e1, cs1.e2, cs.dist)
+    v.addAxis(cs.origin,[cs.dist,cs.dist,cs.dist],cs.rot,label=True,disableCone=True)
+    v.view(interactive=True)
+
 
     return True
 
