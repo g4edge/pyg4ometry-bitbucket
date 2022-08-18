0bc8fbe52bee47902367e3eb6b5f49314e8ac127
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-08-18 23:31:33 +0100

    Development python version coverage

--- test/runCoverage.sh
+++ test/runCoverage.sh
@@ -2,6 +2,6 @@
 
 
 # 1150.988s 2019/10/27
-COVCOMMAND=coverage-3.9
+COVCOMMAND=coverage-3.10
 $COVCOMMAND run --source pyg4ometry --omit="*test*","*gui*","*RegionLexer*","*RegionParser*","*RegionParserVisitor*","*GdmlExpressionLexer*","*GdmlExpressionParser*","*GdmlExpressionParserVisitor*" runTests.py
 $COVCOMMAND report -m > runCoverage.log
