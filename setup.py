004dd5e6a078f7222a67f9ff7ded16f67867f3bf
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-09-21 21:42:31 +0100

    Start of CGAL refactoring

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ from Cython.Build import cythonize
 from distutils.command import build_ext
 from distutils.core import setup, Extension
 
+cgal_extensions = {'pyg4ometry.pycgal.geom':['src/pyg4ometry/pycgal/geom.cxx'],
+                   'pyg4ometry.pycgal.CGAL':['src/pyg4ometry/pycgal/CGAL.cxx'],
+                   'pyg4ometry.pycgal.Surface_mesh':['src/pyg4ometry/pycgal/Surface_mesh.cxx'],
+                   'pyg4ometry.pycgal.Polygon_mesh_processing':['src/pyg4ometry/pycgal/Polygon_mesh_processing.cxx'],
+                   'pyg4ometry.pycgal.Polygon_2':['src/pyg4ometry/pycgal/Polygon_2.cxx']}
 
-cgalExtensions = {'geom':['geom.cxx'],
-                  'algo':['algo.cxx'],
-                  'core':['core.cxx']}
-
-oceExtensions = {'pyg4ometry.pyoce.TCollection':['src/pyg4ometry/pyoce/TCollection.cxx'],
+oce_extensions = {'pyg4ometry.pyoce.TCollection':['src/pyg4ometry/pyoce/TCollection.cxx'],
                  'pyg4ometry.pyoce.TKernel':['src/pyg4ometry/pyoce/TKernel.cxx'],
                  'pyg4ometry.pyoce.TDocStd':['src/pyg4ometry/pyoce/TDocStd.cxx'],
                  'pyg4ometry.pyoce.TDataStd':['src/pyg4ometry/pyoce/TDataStd.cxx'],
@@ -32,17 +33,31 @@ oceExtensions = {'pyg4ometry.pyoce.TCollection':['src/pyg4ometry/pyoce/TCollecti
                  'pyg4ometry.pyoce.XCAFApp':['src/pyg4ometry/pyoce/XCAFApp.cxx'],
                  'pyg4ometry.pyoce.STEPCAFControl':['src/pyg4ometry/pyoce/STEPCAFControl.cxx']}
 
-def cmakeDiscovery() :
+def cmake_discovery() :
     print('running cmake')
     pass
 
-def mesonDiscovery() :
+def meson_discovery() :
+    print('running meson')
     pass
     
-def pybind11CGALExtensions(extDict,depDict) :
-    pass
+def pybind11_CGAL_extensions(extDict,depDict) :
+    extensions = []
+    for ext in extDict :
+        code = extDict[ext]
+        extension = Extension(ext,
+                              include_dirs = ["/opt/local/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/pybind11/include/",
+                                              "/opt/local/include/"],
+                              library_dirs=['/opt/local/lib/'],
+                              libraries = ['mpfr','gmp'],
+                              sources = code,
+                              language="c++",
+                              extra_compile_args=["-std=c++14","-fvisibility=hidden"])
+        extensions.append(extension)
 
-def pybind11OCEExtensions(extDict, depDict) :
+    return extensions
+
+def pybind11_OCE_extensions(extDict, depDict) :
 
     extensions = []
     for ext in extDict :
@@ -60,15 +75,15 @@ def pybind11OCEExtensions(extDict, depDict) :
     return extensions
                               
     
-deps = cmakeDiscovery()
+deps = cmake_discovery()
 
 csgExts  = cythonize(["src/pyg4ometry/pycsg/geom.pyx", "src/pyg4ometry/pycsg/core.pyx"])
-cgalExts = pybind11CGALExtensions(cgalExtensions,deps)
-oceExts  = pybind11OCEExtensions(oceExtensions,deps)
+cgalExts = pybind11_CGAL_extensions(cgal_extensions,deps)
+oceExts  = pybind11_OCE_extensions(oce_extensions,deps)
 
 exts = []
 exts.extend(csgExts)
-# exts.extend(cgalExts)
+exts.extend(cgalExts)
 exts.extend(oceExts)
 
 setup(
