9ebac54aa77e84972268156ab3601b8265aed60e
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-11-04 11:15:28 +0000

    Fix signature for make_square

--- test/pycgal/test_2d_boolean.py
+++ test/pycgal/test_2d_boolean.py
@@ -1,11 +1,11 @@
 import pyg4ometry 
 
-def makeSquare(sizex = 1, sizey = 1, offset = 0):
+def makeSquare(sizex = 1, sizey = 1, offsetx = 0, offsety=0):
 	pa = pyg4ometry.pycgal.Polygon_2.Polygon_2_EPECK()
-	p1 = pyg4ometry.pycgal.Point_2.Point_2_EPECK(0,0)+offset
-	p2 = pyg4ometry.pycgal.Point_2.Point_2_EPECK(0,sizey)+offset
-	p3 = pyg4ometry.pycgal.Point_2.Point_2_EPECK(sizex,sizey)+offset
-	p4 = pyg4ometry.pycgal.Point_2.Point_2_EPECK(sizex,0)+offset
+	p1 = pyg4ometry.pycgal.Point_2.Point_2_EPECK(offsetx,offsety)
+	p2 = pyg4ometry.pycgal.Point_2.Point_2_EPECK(offsetx,sizey+offsety)
+	p3 = pyg4ometry.pycgal.Point_2.Point_2_EPECK(sizex+offsetx,sizey+offsety)
+	p4 = pyg4ometry.pycgal.Point_2.Point_2_EPECK(sizex+offsetx,0)
 
 	pa.push_back(p1)
 	pa.push_back(p2)
@@ -16,8 +16,8 @@ def makeSquare(sizex = 1, sizey = 1, offset = 0):
 
 
 def test_polygon_2_union():
-	pa = makeSquare(pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(0.0,0.0))
-	pb = makeSquare(pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(0.5,0.5))
+	pa = makeSquare(1,1,0,0)
+	pb = makeSquare(1,1,0.5,0.5)
 
 	pr = pyg4ometry.pycgal.Polygon_with_holes_2.Polygon_with_holes_2_EPECK()
 
@@ -26,8 +26,8 @@ def test_polygon_2_union():
 	return pr
 
 def test_polygon_2_intersection():
-	pa = makeSquare(pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(0.0,0.0))
-	pb = makeSquare(pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(0.5,0.5))
+	pa = makeSquare(1,1,0,0)
+	pb = makeSquare(1,1,0.5,0.5)
 
 	pr = pyg4ometry.pycgal.Polygon_with_holes_2.List_Polygon_with_holes_2_EPECK()
 
@@ -37,8 +37,8 @@ def test_polygon_2_intersection():
 
 
 def test_polygon_2_difference():
-	pa = makeSquare(pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(0.0,0.0))
-	pb = makeSquare(pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(0.5,0.5))
+	pa = makeSquare(1,1,0,0)
+	pb = makeSquare(1,1,0.5,0.5)
 
 	pr = pyg4ometry.pycgal.Polygon_with_holes_2.List_Polygon_with_holes_2_EPECK()
 
@@ -47,8 +47,8 @@ def test_polygon_2_difference():
 	return pr
 
 def test_polygon_2_difference_hole():
-	pa = makeSquare(5,5,pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(0.0,0.0))
-	pb = makeSquare(2,2,pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(1,1))
+	pa = makeSquare(5,5,0,0)
+	pb = makeSquare(2,2,1,1)
 
 	pr = pyg4ometry.pycgal.Polygon_with_holes_2.List_Polygon_with_holes_2_EPECK()
 
@@ -58,7 +58,7 @@ def test_polygon_2_difference_hole():
 
 def test_polygon_2_difference_hole_2():
 	pa = test_polygon_2_difference_hole()[0]
-	pb = makeSquare(1,10,pyg4ometry.pycgal.Vector_2.Vector_2_EPECK(2,-3))
+	pb = makeSquare(1,10,2,-3)
 
 	pr = pyg4ometry.pycgal.Polygon_with_holes_2.List_Polygon_with_holes_2_EPECK()
 
